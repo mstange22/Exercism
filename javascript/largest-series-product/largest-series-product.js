1e12510c70dc8cb3f360729ba21eb09b550f3127
Mike S <mstange@dthera.com>
2019-01-06 13:40:15 -0800

    solution

---
+++ javascript/largest-series-product/largest-series-product.js
@@ -0,0 +1,17 @@
+export const largestProduct = (str, num) => {
+// const largestProduct = (str, num) => {
+  if (num < 0) throw new Error('Invalid input.');
+  if (num > str.length) throw new Error('Slice size is too big.');
+  if (num === 0 && str === '') return 1;
+
+  const series = str.split('').map(s => Number(s));
+  return Math.max(...series
+    .reduce((slices, _, i) => i < series.length - num + 1 ? [...slices, series.slice(i, i + num)] : slices, [])
+    .reduce((prods, slice) => [
+      ...prods,
+      slice.reduce((prod, value) => {
+        if (!/\d/.test(value)) throw new Error('Invalid input.');
+        return prod * value;
+      }, 1)
+    ], []));
+};

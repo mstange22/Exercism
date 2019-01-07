6242dc14d82d76ef90fe8efa0e025b415ddd1a81
Mike S <mstange@dthera.com>
2019-01-06 19:47:20 -0800

    solution

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,3 +1,12 @@
 class Leap {
-  // Put your code here
+  bool leapYear(int year) {
+    // return year % 4 != 0 || (year % 100 == 0 && year % 400 != 0) ? false : true;
+    if (year % 4 != 0) {
+      return false;
+    }
+    if (year % 100 != 0) {
+      return true;
+    }
+    return year % 400 == 0;
+  }
 }

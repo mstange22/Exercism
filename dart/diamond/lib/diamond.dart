d26045f39688a260f276a3a7e0b4440e72edc89e
Mike S <mstange@dthera.com>
2019-01-06 22:51:10 -0800

    solution

--- dart/diamond/lib/diamond.dart
+++ dart/diamond/lib/diamond.dart
@@ -1,3 +1,40 @@
 class Diamond {
-  // Put your code here
+  String getSpaces(int spaceCount) {
+    var spaces = '';
+    for (var i = 0; i < spaceCount; i++) {
+      spaces += ' ';
+    }
+    return spaces;
+  }
+
+  List<String> rows(String letter) {
+    List<String> res = [];
+    var diff = letter.codeUnitAt(0) - 'A'.codeUnitAt(0);
+    var outerSpacesCount = diff;
+    var currentLetter = 'A';
+
+    for (var i = 0; i <= 2 * diff; i++) {
+      if (currentLetter == 'A') {
+        res.add('${getSpaces(outerSpacesCount)}A${getSpaces(outerSpacesCount)}');
+      } else if (currentLetter == letter) {
+        // add the middle line
+        res.add('$letter${getSpaces((2 * diff) - 1)}$letter');
+      } else {
+        // add the other lines
+        var innerSpacesCount = (2 * diff) - (outerSpacesCount * 2) - 1;
+        var innerSpaces = getSpaces(innerSpacesCount);
+        var outerSpaces = getSpaces(outerSpacesCount);
+        res.add('$outerSpaces$currentLetter$innerSpaces$currentLetter$outerSpaces');
+      }
+      // update outerSpacesCount & currentLetter
+      if (i < diff) {
+        outerSpacesCount--;
+        currentLetter = String.fromCharCode(currentLetter.codeUnitAt(0) + 1);
+      } else {
+        outerSpacesCount++;
+        currentLetter = String.fromCharCode(currentLetter.codeUnitAt(0) - 1);
+      }
+    }
+    return res;
+  }
 }

e47cefd37aa026233450b4932e03d4318121dc49
Mike S <mstange@dthera.com>
2019-01-07 19:17:01 -0800

    protein_translation solution

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,21 @@
 def proteins(strand):
-    pass
+  m = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine', 'UUC': 'Phenylalanine',
+    'UUA': 'Leucine', 'UUG': 'Leucine',
+    'UCU': 'Serine', 'UCC': 'Serine', 'UCA': 'Serine', 'UCG': 'Serine',
+    'UAU': 'Tyrosine', 'UAC': 'Tyrosine',
+    'UGU': 'Cysteine', 'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP', 'UAG': 'STOP', 'UGA': 'STOP',
+  }
+  proteins = []
+  index = 0
+  while index < len(strand) - 2:
+    if m[strand[index:index+3]] == 'STOP':
+      return proteins
+    if m[strand[index:index+3]]:
+      proteins.append(m[strand[index:index+3]])
+    index += 3
+
+  return proteins

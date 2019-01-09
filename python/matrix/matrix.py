d69d71aa9e84fb8e8e2dcf93d8e7d44c2f093946
Mike S <mstange@dthera.com>
2019-01-09 00:42:12 -0800

    matrix solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,9 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        self.matrix = matrix_string.split('\n')
 
     def row(self, index):
-        pass
+        return [int(digit) for digit in self.matrix[index].split(' ')]
 
     def column(self, index):
-        pass
+        return [int(row.split(' ')[index]) for row in self.matrix]

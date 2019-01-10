7065e808e7b44f603bca1718d9d93a8a53857d76
Mike S <mstange@dthera.com>
2019-01-09 16:17:57 -0800

    bank_account solution (w/o threading)

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,18 +1,34 @@
 class BankAccount(object):
     def __init__(self):
-        pass
+        self.balance = 0
+        self.isOpen = False
 
     def get_balance(self):
-        pass
+        if not self.isOpen:
+            raise ValueError('account is closed')
+        return self.balance
 
     def open(self):
-        pass
+        if self.isOpen:
+            raise ValueError('account already open')
+        self.isOpen = True
 
     def deposit(self, amount):
-        pass
+        if not self.isOpen:
+            raise ValueError('account is closed')
+        if amount < 0:
+            raise ValueError('negative deposit')
+        self.balance += amount
 
     def withdraw(self, amount):
-        pass
+        if amount < 0:
+            raise ValueError('negative withdrawal')
+        if amount > self.balance:
+            raise ValueError('overdrawn')
+        self.balance -= amount
 
     def close(self):
-        pass
+        if not self.isOpen:
+            raise ValueError('account already closed')
+        self.isOpen = False
+        self.balance = 0

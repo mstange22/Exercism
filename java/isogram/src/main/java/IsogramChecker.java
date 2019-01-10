d148475742907d0f9ff27833f9221bc78a381b89
Mike S <mstange@dthera.com>
2019-01-09 17:36:12 -0800

    isogram solution

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class IsogramChecker {
-
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Map hm = new HashMap();
+        for (int i = 0; i < phrase.length(); i++) {
+            char c = Character.toLowerCase(phrase.charAt(i));
+            if (c >= 'a' && c <= 'z') {
+                if (hm.containsKey(c)) {
+                    return false;
+                }
+                hm.put(c, true);
+            }
+        }
+        return true;
     }
-
 }

6434ce9314506871b30f159fe88305833c965e5b
Mike S <mstange@dthera.com>
2019-01-16 08:00:19 -0800

    revised isogram solution

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -4,15 +4,15 @@ import "strings"
 
 // IsIsogram determines if a word has a repeating char.
 func IsIsogram(s string) bool {
-	charMap := map[string]int{}
+	charMap := map[string]bool{}
 
 	for i := range s {
 		newChar := strings.ToLower(string(s[i]))
 		if newChar != "-" && newChar != " " {
-			if charMap[newChar] != 0 {
+			if charMap[newChar] {
 				return false
 			}
-			charMap[newChar] = 1
+			charMap[newChar] = true
 		}
 	}
 

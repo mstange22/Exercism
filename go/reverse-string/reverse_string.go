4b91403dcd18fd98b92fde2751093ae3bfcf48da
Mike S <mstange@dthera.com>
2019-01-16 11:50:18 -0800

    reverse_string solution

---
+++ go/reverse-string/reverse_string.go
@@ -0,0 +1,10 @@
+package reverse
+
+// String reverses a string
+func String(s string) string {
+	chars := []rune(s)
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
+	return string(chars)
+}

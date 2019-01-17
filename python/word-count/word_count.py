a926b796edeeb3e54d21c755e877d72bc27742b4
Mike S <mstange@dthera.com>
2019-01-17 09:51:57 -0800

    revised word_count solution

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,8 +1,5 @@
+from collections import Counter
 import re
-from collections import defaultdict
 
 def word_count(phrase):
-  word_map = defaultdict(int)
-  for word in re.findall(r'([a-z]+\'{1}[a-z]+|[a-z0-9]+)', phrase.lower()):
-      word_map[word] += 1
-  return word_map
+  return Counter(word for word in re.findall(r'([a-z]+\'{1}[a-z]+|[a-z0-9]+)', phrase.lower()))

9451ffb66eca630e2aedc61148805eba788280c9
Mike S <mstange@dthera.com>
2019-01-18 12:55:16 -0800

    revised error_handling solution

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -14,7 +14,7 @@ def handle_error_by_returning_tuple(input_data):
     return False, None
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-  try:
-    filelike_object.do_something()
-  finally:
-    filelike_object.close()
+  with filelike_object as f:
+    f.open()
+    f.do_something()
+    f.close()

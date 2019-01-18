f9053fc2ccee40e07ba8b17de255acb585d8fb50
Mike S <mstange@dthera.com>
2019-01-18 08:36:46 -0800

    revised difference_of_squares solution

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,8 +1,8 @@
 def square_of_sum(count):
-  return sum(range(1, count+1))**2
+  return (count * (count + 1) / 2)**2
 
 def sum_of_squares(count):
-  return sum(i**2 for i in range(1, count+1))
+  return (count * (count + 1) * ((2 * count) + 1)) / 6
 
 def difference(count):
   return square_of_sum(count) - sum_of_squares(count)

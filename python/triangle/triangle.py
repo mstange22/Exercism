47f673dfc9941678140fbb77f438e75d3bcb3345
Mike S <mstange@dthera.com>
2019-01-18 16:12:06 -0800

    revised triangle solution

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,16 +1,14 @@
+def validator(func):
+  return lambda x: 0 not in x and sum(x) - max(x) > max(x) and func(x)
+
+@validator
 def is_equilateral(sides):
-  if 0 in sides:
-    return False
   return len(set(sides)) == 1
 
+@validator
 def is_isosceles(sides):
-  if 0 in sides:
-    return False
-  a, b, c = sides[0], sides[1], sides[2]
-  return (a == b or a == c or b == c) and a + b >= c and a + c >= b and b + c >= a
+  return len(set(sides)) <= 2
 
+@validator
 def is_scalene(sides):
-  if 0 in sides:
-    return False
-  a, b, c = sides[0], sides[1], sides[2]
-  return sorted(list(set(sides))) == sorted(sides) and a + b >= c and a + c >= b and b + c >= a
+  return len(set(sides)) == 3

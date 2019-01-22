62561ddc7e13da5ea5e16e3e842f14af327e28b0
Mike S <mstange@dthera.com>
2019-01-22 10:58:42 -0800

    revised triangle solution

--- cpp/triangle/triangle.cpp
+++ cpp/triangle/triangle.cpp
@@ -1,14 +1,12 @@
 #include "triangle.h"
 #include <stdexcept>
 
-using namespace std;
-
 triangle::flavor triangle::kind(double a, double b, double c) {
   if (a <= 0 || b <= 0 || c <= 0) {
-    throw domain_error("triangles with no size are illegal");
+    throw std::domain_error("triangles with no size are illegal");
   }
   if (a + b < c || a + c < b || b + c < a) {
-    throw domain_error("triangle inequality violation");
+    throw std::domain_error("triangle inequality violation");
   }
   if (a + b == c || a + c == b || b + c == a) {
     return triangle::flavor::degenerate;
--- cpp/triangle/triangle.h
+++ cpp/triangle/triangle.h
@@ -1,6 +1,5 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
-#include <string>
 
 namespace triangle {
   enum flavor { scalene, isosceles, equilateral, degenerate };

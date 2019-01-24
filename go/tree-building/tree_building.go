627a7dae65b48289e2a3f56fda05e02eb3ab05c1
Mike S <mstange@dthera.com>
2019-01-24 12:42:35 -0800

    refactored tree_building solution

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -22,44 +22,32 @@ func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
-
-	// sort records on Parent, then on ID
 	sort.Slice(records, func(i, j int) bool {
-		if records[i].Parent != records[j].Parent {
-			return records[i].Parent < records[j].Parent
-		}
 		return records[i].ID < records[j].ID
 	})
-
 	if records[0].Parent != records[0].ID {
 		return nil, errors.New("root node has parent")
 	}
 
-	// initialize Node pointer array
+	// construct array-based implementation of Node pointer tree
 	Nodes := make([]*Node, len(records))
 
-	// traverse records, adding Node pointers to Node, then check for children
+	// traverse records, adding Node pointers to Nodes
+	// add current node to its parent's Children array
 	for i := 0; i < len(records); i++ {
 		if records[i].ID != 0 && records[i].ID <= records[i].Parent {
 			return nil, errors.New("cycle")
 		}
-		if Nodes[records[i].ID] == nil {
-			newNode := Node{ID: records[i].ID}
-			Nodes[records[i].ID] = &newNode
+		if records[i].ID != i {
+			return nil, errors.New("duplicate node")
 		}
-		// pass through remaining records, adding child node pointers to Nodes[j] and Nodes[i].Children
-		for j := i + 1; j < len(records); j++ {
-			if records[j].Parent == records[i].ID {
-				if records[j].ID >= len(Nodes) {
-					return nil, errors.New("non-continuous")
-				}
-				if Nodes[records[j].ID] != nil {
-					return nil, errors.New("duplicate node")
-				}
-				newNode := Node{ID: records[j].ID}
-				Nodes[records[j].ID] = &newNode
-				Nodes[i].Children = append(Nodes[i].Children, Nodes[records[j].ID])
-			}
+
+		newNode := Node{ID: i}
+		Nodes[i] = &newNode
+
+		if i != 0 {
+			parentID := records[i].Parent
+			Nodes[parentID].Children = append(Nodes[parentID].Children, Nodes[i])
 		}
 	}
 	return Nodes[0], nil

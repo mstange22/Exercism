b2d2f2e3508201ff5d313a87e0fe0b4ac5d41c3d
Mike S <mstange@dthera.com>
2019-01-27 10:41:26 -0800

    revised robot_name solution

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,26 +1,35 @@
 package robotname
 
-import "math/rand"
+import (
+	"errors"
+	"math/rand"
+)
 
 // Robot is a robot.
 type Robot struct {
 	name string
 }
 
-var names = map[string]bool{}
+var names = map[string]bool{"": true}
 
 // Name defines and returns a robot's name.
 func (r *Robot) Name() (string, error) {
-	if r.name == "" {
+	if r.name != "" {
+		return r.name, nil
+	}
+	if len(names) == maxNames+1 {
+		return "", errors.New("namespace is exhausted")
+	}
+
+	for names[r.name] {
 		r.name += string(rand.Intn(26) + 'A')
 		r.name += string(rand.Intn(26) + 'A')
 		r.name += string(rand.Intn(10) + '0')
 		r.name += string(rand.Intn(10) + '0')
 		r.name += string(rand.Intn(10) + '0')
 	}
-	if names[r.name] {
-		r.Reset()
-	}
+
+	names[r.name] = true
 	return r.name, nil
 }
 

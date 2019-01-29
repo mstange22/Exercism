3352cc5529ca26548a36f95419ca0645bb510189
Mike S <mstange@dthera.com>
2019-01-29 14:46:51 -0800

    dnd-character solution

--- java/dnd-character/src/main/java/DnDCharacter.java
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -1,11 +1,75 @@
+import java.lang.Math;
+
 class DnDCharacter {
+    private int _strength;
+    private int _dexterity;
+    private int _constitution;
+    private int _intelligence;
+    private int _wisdom;
+    private int _charisma;
+    private int _constitutionModifier;
+    private int _hitpoints;
+    
+    DnDCharacter() {
+        _strength = getRandom();
+        _dexterity = getRandom();
+        _constitution = getRandom();
+        _intelligence = getRandom();
+        _wisdom = getRandom();
+        _charisma = getRandom();
+        _constitutionModifier = Math.floorDiv((_constitution - 10), 2);
+        _hitpoints = 10 + _constitutionModifier;
+    }
 
     int ability() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return getRandom();
     }
 
     int modifier(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        _constitution = input;
+        _constitutionModifier = Math.floorDiv((_constitution - 10), 2);
+        _hitpoints = 10 + _constitutionModifier;
+        return _constitutionModifier;
     }
 
+    int getRandom() {
+        int sum = 0;
+        int min = 7;
+        for (int i = 0; i < 4; i++) {
+            int roll = (int)(Math.random() * 6) + 1;
+            sum += roll;
+            if (roll < min) {
+                min = roll;
+            }
+        }
+        return sum - min;
+    }
+
+    int getStrength() {
+        return _strength;
+    }
+
+    int getDexterity() {
+        return _dexterity;
+    }
+
+    int getConstitution() {
+        return _constitution;
+    }
+
+    int getIntelligence() {
+        return _intelligence;
+    }
+
+    int getWisdom() {
+        return _wisdom;
+    }
+
+    int getCharisma() {
+        return _charisma;
+    }
+
+    int getHitpoints() {
+        return _hitpoints;
+    }
 }

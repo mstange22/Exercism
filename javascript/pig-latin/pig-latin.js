12da5c792d0ee4bbeacd66652b10d8d02ffa0848
Mike S <mstange@dthera.com>
2019-01-31 09:00:01 -0800

    revised pig-latin solution

--- javascript/pig-latin/pig-latin.js
+++ javascript/pig-latin/pig-latin.js
@@ -1,8 +1,8 @@
-const translateWord = (word) => {
+const translate = (word) => {
   if (/^([aeiou]|xr|yt)/.test(word)) return word + 'ay';
   return word.replace(/^([^aeiou]+(?=y)|[^aeiou]*qu|[^aeiou]+)([a-z]+)/, '$2$1') + 'ay';
 };
 
 export const translator = {
-  translate: (phrase) => phrase.split(' ').map(translateWord).join(' ')
+  translate: (phrase) => phrase.split(' ').map(translate).join(' ')
 };

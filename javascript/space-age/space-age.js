4c73dc6d95b31655de152feaeddfed376ee17f7a
Mike S <mstange@dthera.com>
2019-01-30 16:22:34 -0800

    revised space-age solution

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -7,40 +7,29 @@ const SATURN_YEAR_IN_EARTH_YEARS = 29.447498
 const URANUS_YEAR_IN_EARTH_YEARS = 84.016846
 const NEPTUNE_YEAR_IN_EARTH_YEARS = 164.79132
 
-export class SpaceAge {
-  constructor(seconds) {
-    this.seconds = seconds;
-  }
+const convert = (seconds, conversionFactor) => {
+  return +(seconds / EARTH_YEAR_IN_SECONDS / conversionFactor).toFixed(2);
+};
 
-  onEarth() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS).toFixed(2));
+export const age = (planet, seconds) => {
+  switch(planet) {
+    case 'earth':
+      return +((seconds / EARTH_YEAR_IN_SECONDS).toFixed(2));
+    case 'mercury':
+      return convert(seconds, MERCURY_YEAR_IN_EARTH_YEARS);
+    case 'venus':
+      return convert(seconds, VENUS_YEAR_IN_EARTH_YEARS);
+    case 'mars':
+      return convert(seconds, MARS_YEAR_IN_EARTH_YEARS);
+    case 'jupiter':
+      return convert(seconds, JUPITER_YEAR_IN_EARTH_YEARS);
+    case 'saturn':
+      return convert(seconds, SATURN_YEAR_IN_EARTH_YEARS);
+    case 'uranus':
+      return convert(seconds, URANUS_YEAR_IN_EARTH_YEARS);
+    case 'neptune':
+      return convert(seconds, NEPTUNE_YEAR_IN_EARTH_YEARS);
+    default:
+      return null;
   }
-
-  onMercury() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS / MERCURY_YEAR_IN_EARTH_YEARS).toFixed(2))
-  }
-
-  onVenus() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS / VENUS_YEAR_IN_EARTH_YEARS).toFixed(2))
-  }
-
-  onMars() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS / MARS_YEAR_IN_EARTH_YEARS).toFixed(2))
-  }
-
-  onJupiter() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS / JUPITER_YEAR_IN_EARTH_YEARS).toFixed(2))
-  }
-
-  onSaturn() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS / SATURN_YEAR_IN_EARTH_YEARS).toFixed(2))
-  }
-
-  onUranus() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS / URANUS_YEAR_IN_EARTH_YEARS).toFixed(2))
-  }
-
-  onNeptune() {
-    return +((this.seconds / EARTH_YEAR_IN_SECONDS / NEPTUNE_YEAR_IN_EARTH_YEARS).toFixed(2))
-  }
-}
+};

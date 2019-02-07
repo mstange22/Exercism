4f4234d1fade2a804d27d331d4888002f54dc983
Mike S <mstange@dthera.com>
2019-02-06 20:01:10 -0800

    revised raindrops solution

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -1,16 +1,16 @@
 #include "raindrops.h"
+#include <map>
 
-rain MAP[3] = {
-  { 3, "Pling" },
-  { 5, "Plang" },
-  { 7, "Plong" },
-};
+std::map<int, std::string> rain {{3, "Pling"}, {5, "Plang"}, {7, "Plong"}};
 
 std::string raindrops::convert(int n) {
-  std::string res = "";
-  for (int i = 0; i < 3; i++) {
-    if (n % MAP[i].mod == 0) {
-      res += MAP[i].sound;
+  std::string res;
+  std::map<int, std::string>::iterator it;
+
+  for (it = rain.begin(); it != rain.end(); it++) {
+    int key = it->first;
+    if (n % key == 0) {
+      res += rain[key];
     }
   }
   return res.length() == 0 ? std::to_string(n) : res;
--- cpp/raindrops/raindrops.h
+++ cpp/raindrops/raindrops.h
@@ -2,11 +2,6 @@
 #define RAINDROPS_H
 #include <string>
 
-struct rain {
-  int mod;
-  std::string sound;
-};
-
 namespace raindrops {
   std::string convert(int);
 }

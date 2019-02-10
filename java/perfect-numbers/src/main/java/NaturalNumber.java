b87b8bc9dc2c131a8e45ad60287f98991ddc995a
Mike S <mstange@dthera.com>
2019-02-10 13:52:34 -0800

    revised perfect-number solution

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,29 +1,19 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 class NaturalNumber {
-  private List<Integer> factors = new ArrayList<Integer>();
+
   private Classification classification;
 
   NaturalNumber(int n) {
     if (n < 1) {
       throw new IllegalArgumentException("You must supply a natural number (positive integer)");
     }
-    if (n == 1) {
+
+    int sum = IntStream.rangeClosed(1, n / 2).filter(i -> n % i == 0).sum();
+
+    if (sum == 1) {
       this.classification = Classification.DEFICIENT;
-      return;
-    }
-    factors.add(1);
-    int sum = 0;
-    for (int i = 2; i <= n / 2; i++) {
-      if (n % i == 0) {
-        factors.add(i);
-      }
-    }
-    for (int i = 0; i < factors.size(); i++) {
-      sum += factors.get(i);
-    }
-    if (sum == n) {
+    } else if (sum == n) {
       this.classification = Classification.PERFECT;
     } else if (sum > n) {
       this.classification = Classification.ABUNDANT;
@@ -35,4 +25,5 @@ class NaturalNumber {
   public Classification getClassification() {
     return this.classification;
   }
+
 }

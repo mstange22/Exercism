65e2417e5e4165a478ae3b41f13edcb6be8c83c6
Mike S <mstange@dthera.com>
2019-02-15 18:37:33 -0800

    sieve solution

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,11 +1,29 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 class Sieve {
+    private final List<Integer> primes = new ArrayList<Integer>();
+
+    // flag composite numbers with map 
+    private final Map<Integer, Boolean> nonPrimes = new HashMap<Integer, Boolean>();
+
     Sieve(int maxPrime) {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+        // increment until find next available unflagged number
+        for (int i = 2; i <= maxPrime; i++) {
+            if (!nonPrimes.containsKey(i)) {
+                // first unflagged number is prime
+                primes.add(i);
+                // mark all multiples as not prime
+                for (int j = i + i; j <= maxPrime; j += i) {
+                    nonPrimes.put(j, true);
+                }
+            }
+        }
     }
 
     List<Integer> getPrimes() {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+        return this.primes;
     }
 }

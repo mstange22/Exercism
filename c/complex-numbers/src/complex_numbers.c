5ce4def421eea13f903ab126b4ee976cbf64c8b8
Mike S <mstange@dthera.com>
2019-02-27 11:55:24 -0800

    revised complex_numbers solution

--- c/complex-numbers/src/complex_numbers.c
+++ c/complex-numbers/src/complex_numbers.c
@@ -1,44 +1,36 @@
 #include "complex_numbers.h"
-#include <stdio.h>
 #include <math.h>
 
 complex_t c_add(complex_t a, complex_t b)
 {
-   // (a + c) + (b + d) * i
-   complex_t res = {
+   return (complex_t) {
       a.real + b.real,
       a.imag + b.imag,
    };
-   return res;
 }
 
 complex_t c_sub(complex_t a, complex_t b)
 {
-   complex_t res = {
+   return (complex_t) {
       a.real - b.real,
       a.imag - b.imag,
    };
-   return res;
 }
 
 complex_t c_mul(complex_t a, complex_t b)
 {
-   // (a * c - b * d) + (b * c + a * d) * i
-   complex_t res = {
+   return (complex_t) {
       (a.real * b.real) - (a.imag * b.imag),
       (a.imag * b.real) + (a.real * b.imag),
    };
-   return res;
 }
 
 complex_t c_div(complex_t a, complex_t b)
 {
-   // (a * c + b * d)/(c^2 + d^2) + (b * c - a * d)/(c^2 + d^2) * i
-   complex_t res = {
+   return (complex_t) {
       ((a.real * b.real) + (a.imag * b.imag)) / (b.real * b.real + b.imag * b.imag),
       ((a.imag * b.real) - (a.real * b.imag)) / (b.real * b.real + b.imag * b.imag),
    };
-   return res;
 }
 
 double c_abs(complex_t x)
@@ -48,11 +40,7 @@ double c_abs(complex_t x)
 
 complex_t c_conjugate(complex_t x)
 {
-   complex_t res = {
-      x.real,
-      -x.imag,
-   };
-   return res;
+   return (complex_t) { x.real, -x.imag };
 }
 
 double c_real(complex_t x)
@@ -67,10 +55,8 @@ double c_imag(complex_t x)
 
 complex_t c_exp(complex_t x)
 {
-   // cos(b) + i * sin(b)
-   complex_t res = {
+   return (complex_t) {
       exp(x.real) * cos(x.imag),
       exp(x.real) * sin(x.imag),
    };
-   return res;
 }

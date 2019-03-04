f83ec307865fb65958ddaf38274485c105f25bcd
Mike S <mstange@dthera.com>
2019-03-04 14:36:24 -0800

    refactored rotational-cipher solution

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,16 +1,18 @@
-const RotationalCipher = {
+const convert = (char, rot) => {
+  const A = 'A'.charCodeAt();
+  const upperCode = char.toUpperCase().charCodeAt();
+  const shift = ((upperCode - A + rot) % 26) + A - upperCode;
+  return String.fromCharCode((char.charCodeAt() + shift));
+};
+
+export default {
   rotate: (s, rot) => {
     let res = '';
     for (let i = 0; i < s.length; i++) {
-      if (!/[a-zA-Z]/.test(s[i])) res += s[i];
-      else res += String.fromCharCode(s[i].charCodeAt()
-        + ((
-            (s[i].toUpperCase().charCodeAt() - 'A'.charCodeAt()) + rot) % 26)
-            - (s[i].toUpperCase().charCodeAt() - 'A'.charCodeAt()
-        ));
+      let c = s[i];
+      if (/[a-zA-Z]/.test(c)) c = convert(c, rot);
+      res += c;
     }
     return res;
   },
 };
-
-export default RotationalCipher;

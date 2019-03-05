ab181b12b0fb8db816333121e79385ae63445d34
Mike S <mstange@dthera.com>
2019-03-04 17:55:39 -0800

    refactored say solution

--- python/say/say.py
+++ python/say/say.py
@@ -1,92 +1,41 @@
-def convert_teens(n):
-  if n == 11:
-    return 'eleven'
-  if n == 12:
-    return 'twelve'
-  if n == 13:
-    return 'thirteen'
-  if n == 14:
-    return 'fourteen'
-  if n == 15:
-    return 'fifteen'
-  if n == 16:
-    return 'sixteen'
-  if n == 17:
-    return 'seventeen'
-  if n == 18:
-    return 'eighteen'
-  return 'nineteen'
+ONES = {
+  1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+  6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+}
+
+TEENS = {
+  11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
+  16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen',
+}
+
+TENS = {
+  1: 'ten', 2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty',
+  6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety',
+}
 
 def convert_ones(n):
   ones = n % 10
-  if ones == 9:
-    return 'nine'
-  if ones == 8:
-    return 'eight'
-  if ones == 7:
-    return 'seven'
-  if ones == 6:
-    return 'six'
-  if ones == 5:
-    return 'five'
-  if ones == 4:
-    return 'four'
-  if ones == 3:
-    return 'three'
-  if ones == 2:
-    return 'two'
-  if ones == 1:
-    return 'one'
-  return ''
+  return ONES[ones] if ones >= 1 and ones <= 9 else ''
 
 def convert_tens(n):
-  print('tens:', n)
   tens = n // 10
-  if tens == 9:
-    return 'ninety'
-  if tens == 8:
-    return 'eighty'
-  if tens == 7:
-    return 'seventy'
-  if tens == 6:
-    return 'sixty'
-  if tens == 5:
-    return 'fifty'
-  if tens == 4:
-    return 'forty'
-  if tens == 3:
-    return 'thirty'
-  if tens == 2:
-    return 'twenty'
-  if tens == 1:
-    return 'ten'
-  return ''
+  return TENS[tens] if tens >= 1 else ''
 
 def convert_hundred(n):
-  # teens
   if n >= 11 and n <= 19:
-    return convert_teens(n)
-  # ones
+    return TEENS[n]
   if n >= 1 and n <= 9:
-    return convert_ones(n)
-  # tens
-  print('in hundreds:', n)
+    return ONES[n]
   return convert_tens(n) + ('' if n % 10 == 0 else '-') + convert_ones(n)
 
 def convert_thousand(n):
-  print('in thousand:', n)
   res = ''
-  # hundreds
   if n >= 100:
     res += convert_ones(n // 100) + ' hundred' + ('' if n % 100 == 0 else ' and ')
-    print('in thousand:', res)
     n %= 100
-  
-  # last hundred
   return res + convert_hundred(n)
 
 def say(number):
-  print('say:', number)
   if number < 0 or number >= 1e12:
     raise ValueError('Number must be between 0 and 999,999,999,999.')
   if number == 0:

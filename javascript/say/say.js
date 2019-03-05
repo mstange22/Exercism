4099fdc22e7542468de050a967e659d134f21105
Mike S <mstange@dthera.com>
2019-03-04 20:40:41 -0800

    refactored say solution

--- javascript/say/say.js
+++ javascript/say/say.js
@@ -1,86 +1,41 @@
-const convertTeens = (num) => {
-  if (num === 11) return 'eleven';
-  if (num === 12) return 'twelve';
-  if (num === 13) return 'thirteen';
-  if (num === 14) return 'fourteen';
-  if (num === 15) return 'fifteen';
-  if (num === 16) return 'sixteen';
-  if (num === 17) return 'seventeen';
-  if (num === 18) return 'eighteen';
-  return 'nineteen';
-}
+const TEENS = {
+  11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen',
+};
 
-const convertOnes = (num) => {
-  const ones = num % 10;
-  if (ones === 9) return 'nine';
-  if (ones === 8) return 'eight';
-  if (ones === 7) return 'seven';
-  if (ones === 6) return 'six';
-  if (ones === 5) return 'five';
-  if (ones === 4) return 'four';
-  if (ones === 3) return 'three';
-  if (ones === 2) return 'two';
-  if (ones === 1) return 'one';
-  return '';
-}
+const ONES = {
+  1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+};
 
-const convertTens = (num) => {
-  const tens = Math.trunc(num / 10);
-  if (tens === 9) return 'ninety';
-  if (tens === 9) return 'ninety';
-  if (tens === 8) return 'eighty';
-  if (tens === 7) return 'seventy';
-  if (tens === 6) return 'sixty';
-  if (tens === 5) return 'fifty';
-  if (tens === 4) return 'forty';
-  if (tens === 3) return 'thirty';
-  if (tens === 2) return 'twenty';
-  if (tens === 1) return 'ten';
-  return '';
-}
+const TENS = {
+  1: 'ten', 2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty', 6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety',
+};
 
-const convertHundred = (num) => {
-  // teens
-  if (num >= 11 && num <= 19) return convertTeens(num);
-  // ones
-  if (num >= 1 && num <= 9) return convertOnes(num);
-  // tens
-  return convertTens(num) + (num % 10 === 0 ? '' : '-' + convertOnes(num));
-}
-
-const convertThousand = (num) => {
-  let returnString = '';
-  // hundreds
-  if (num >= 100) {
-    returnString += (convertOnes(Math.trunc(num / 100)) + ' hundred' + (num % 100 === 0 ? '' : ' '));
-    num %= 100;
-  }
-  // last hundred
-  return returnString + convertHundred(num);
-}
+const BIG_NUMS = {
+  1e9: 'billion', 1e6: 'million', 1000: 'thousand', 100: 'hundred',
+};
 
 export class Say {
-  inEnglish(num) {
-    if (num < 0 || num >= 1e12) throw new Error('Number must be between 0 and 999,999,999,999.');
-    if (num === 0) return 'zero';
-    let returnString = '';
+  inEnglish(n) {
+    if (n < 0 || n >= 1e12) throw new Error('Number must be between 0 and 999,999,999,999.');
+    if (n === 0) return 'zero';
+    if (n >= 11 && n <= 19) return TEENS[n];
+    if (n >= 1 && n <= 9) return ONES[n];
 
-    // billions
-    if (num >= 1e9) {
-      returnString += (convertThousand(Math.trunc(num / (1e9))) + ' billion' + (num % (1e9) === 0 ? '' : ' '));
-      num %= 1e9;
-    }
-    // millions
-    if (num >= 1e6) {
-      returnString += (convertThousand(Math.trunc(num / (1e6))) + ' million' + (num % (1e6) === 0 ? '' : ' '));
-      num %= 1e6;
-    }
-    // thousands
-    if (num >= 1000) {
-      returnString += (convertThousand(Math.trunc(num / 1000)) + ' thousand' + (num % 1000 === 0 ? '' : ' '));
-      num %= 1000;
-    }
-    // last thousand
-    return returnString + convertThousand(num);
+    let res = '';
+
+    Object.keys(BIG_NUMS)
+      .sort((a, b) => b - a)
+      .forEach((key) => {
+        if (n >= Number(key)) {
+          res += `${this.inEnglish(Math.trunc(n / Number(key)))} ${BIG_NUMS[key]}${n % key === 0 ? '' : ' '}`;
+          n %= key;
+        }
+      });
+    
+    if (n > 0 && n < 20) return res + this.inEnglish(n);
+
+    const tens = Math.floor(n / 10);
+    const ones = n % 10;
+    return `${res}${tens >= 1 ? TENS[tens] : ''}${ones === 0 ? '' : `-${ONES[ones]}`}`;
   }
 }

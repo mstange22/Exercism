5165c50a60a8945e7d0ed45c8a271625bf390b2d
Mike S <mstange@dthera.com>
2019-03-06 10:13:02 -0800

    revised binary-search solution

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -1,16 +1,24 @@
 export class BinarySearch {
   constructor(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) return;
+      if (arr[i] > arr[i + 1]) {
+        return;
+      }
     }
     this.array = arr;
   }
 
   indexOf(n, min = 0, max = this.array.length - 1) {
-    if (min > max) return -1;
+    if (min > max) {
+      return -1;
+    }
     const mid = Math.floor((min + max) / 2);
-    if (this.array[mid] === n) return mid;
-    if (n > this.array[mid]) return this.indexOf(n, mid + 1, max);
+    if (this.array[mid] === n) {
+      return mid;
+    }
+    if (n > this.array[mid]) {
+      return this.indexOf(n, mid + 1, max);
+    }
     return this.indexOf(n, min, min - 1);
   }
 }

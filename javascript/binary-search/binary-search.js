4b238222348a968cac6ec41a9563c10846071519
Mike S <mstange@dthera.com>
2019-03-06 10:01:25 -0800

    recursive binary-search solution

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -1,26 +1,16 @@
 export class BinarySearch {
   constructor(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        return;
-      }
+      if (arr[i] > arr[i + 1]) return;
     }
     this.array = arr;
   }
 
-  indexOf(n) {
-    let min = 0;
-    let max = this.array.length - 1;
-    while (min !== max) {
-      const mid = Math.floor((min + max) / 2);
-      if (this.array[mid] === n) return mid;
-      if (n > this.array[mid]) {
-        min = mid + 1;
-      } else {
-        max = min - 1;
-      }
-    }
-    if (this.array[min] === n) return min;
-    return -1;
+  indexOf(n, min = 0, max = this.array.length - 1) {
+    if (min > max) return -1;
+    const mid = Math.floor((min + max) / 2);
+    if (this.array[mid] === n) return mid;
+    if (n > this.array[mid]) return this.indexOf(n, mid + 1, max);
+    return this.indexOf(n, min, min - 1);
   }
 }

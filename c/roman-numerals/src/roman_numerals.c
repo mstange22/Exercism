96df55430abe65a2ebffbb6dc4b0dfe901305422
Mike S <mstange@dthera.com>
2019-03-15 11:32:38 -0700

    null terminators and malloc null check

--- c/roman-numerals/src/roman_numerals.c
+++ c/roman-numerals/src/roman_numerals.c
@@ -6,28 +6,29 @@
 
 typedef struct {
   int arabic;
-  char roman[2];
+  char roman[3];
 } roman_map;
 
 roman_map ROMAN_NUMERALS[NUM_ROMAN] = {
-  { 1000, { "M" } },
-  { 900, { "CM" } },
-  { 500, { "D" } },
-  { 400, { "CD" } },
-  { 100, { "C" } },
-  { 90, { "XC" } },
-  { 50, { "L" } },
-  { 40, { "XL" } },
-  { 10, { "X" } },
-  { 9, { "IX" } },
-  { 5, { "V" } },
-  { 4, { "IV" } },
-  { 1, { "I" } },
+  { 1000, { "M\0" } },
+  { 900, { "CM\0" } },
+  { 500, { "D\0" } },
+  { 400, { "CD\0" } },
+  { 100, { "C\0" } },
+  { 90, { "XC\0" } },
+  { 50, { "L\0" } },
+  { 40, { "XL\0" } },
+  { 10, { "X\0" } },
+  { 9, { "IX\0" } },
+  { 5, { "V\0" } },
+  { 4, { "IV\0" } },
+  { 1, { "I\0" } },
 };
 
 char *to_roman_numeral(int n)
 {
   char *res = malloc(1);
+  if (res == NULL) return NULL;
   res[0] = '\0';
 
   for (int i = 0; i < NUM_ROMAN; i++)

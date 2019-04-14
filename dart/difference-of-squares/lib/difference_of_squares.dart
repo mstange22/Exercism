ca0810afbf8a63d88aec3aee1102aa817e675370
Mike S <mstange@dthera.com>
2019-04-14 11:51:29 -0400

    int div and use var for local variables

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,13 +1,13 @@
 class DifferenceOfSquares {
   int squareOfSum(int n) {
     // n(n + 1)/2
-    int sum = ((n * (n + 1))/2).round();
+    var sum = ((n * (n + 1)) ~/ 2);
     return sum * sum;
   }
 
   int sumOfSquare(int n) {
     // n(n + 1)(2n + 1)/6
-    return ((n * (n + 1) * ((2 * n) + 1)) / 6).round();
+    return ((n * (n + 1) * ((2 * n) + 1)) ~/ 6);
   }
 
   int differenceOfSquares(int n) {

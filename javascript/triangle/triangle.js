c4d477b1202cbec33ad3858c451701a917c5bedb
Mike S <mstange@dthera.com>
2019-04-19 09:47:17 -0700

    revised triangle solution

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -4,12 +4,21 @@ class Triangle {
     this.b = b;
     this.c = c;
   }
+
   kind() {
-    if (this.a <= 0 || this.b <= 0 || this.c <= 0) throw 'triangles with no zero or negative sides are illegal';
-    if (this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) throw 'triangles violating triangle inequality are illegal';
-    if (this.a === this.b && this.b === this.c) return 'equilateral';
-    if (this.a == this.b || this.a === this.c || this.b === this.c) return 'isosceles';
-    return "scalene";
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('triangles with zero or negative sides are illegal');
+    }
+    if (this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) {
+      throw new Error('triangles violating triangle inequality are illegal');
+    }
+    if (this.a === this.b && this.b === this.c) {
+      return 'equilateral';
+    }
+    if (this.a === this.b || this.a === this.c || this.b === this.c) {
+      return 'isosceles';
+    }
+    return 'scalene';
   }
 }
 

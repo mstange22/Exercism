f7234f7109b5e344b04f435b5bc49801d9c414d5
Mike S <mstange@dthera.com>
2019-04-20 08:31:09 -0700

    queen_attack solution

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,6 +1,22 @@
 class Queen(object):
     def __init__(self, row, column):
-        pass
+        if row < 0 or row > 7:
+            raise ValueError('bad row value')
+        if column < 0 or column > 7:
+            raise ValueError('bad column value')
+        self.row = row
+        self.column = column
 
     def can_attack(self, another_queen):
-        pass
+        if self.row == another_queen.row and self.column == another_queen.column:
+            raise ValueError('same position')
+
+        # orthogonal
+        if self.row == another_queen.row or self.column == another_queen.column:
+            return True
+        
+        # diagonal
+        if abs(self.row - another_queen.row) == abs(self.column - another_queen.column):
+            return True
+        
+        return False
--- python/sgf-parsing/sgf_parsing_test.py
+++ python/sgf-parsing/sgf_parsing_test.py
@@ -67,19 +67,19 @@ class SgfParsingTest(unittest.TestCase):
         )
         self.assertEqual(parse(input_string), expected)
 
-    # def test_multiple_property_values(self):
-    #     input_string = '(;A[b][c][d])'
-    #     expected = SgfTree(
-    #         properties={'A': ['b', 'c', 'd']}
-    #     )
-    #     self.assertEqual(parse(input_string), expected)
+    def test_multiple_property_values(self):
+        input_string = '(;A[b][c][d])'
+        expected = SgfTree(
+            properties={'A': ['b', 'c', 'd']}
+        )
+        self.assertEqual(parse(input_string), expected)
 
-    # def test_escaped_property(self):
-    #     input_string = '(;A[\\]b\nc\nd\t\te \n\\]])'
-    #     expected = SgfTree(
-    #         properties={'A': [']b\nc\nd  e \n]']}
-    #     )
-    #     self.assertEqual(parse(input_string), expected)
+    def test_escaped_property(self):
+        input_string = '(;A[\\]b\nc\nd\t\te \n\\]])'
+        expected = SgfTree(
+            properties={'A': [']b\nc\nd  e \n]']}
+        )
+        self.assertEqual(parse(input_string), expected)
 
     def test_multiple_properties(self):
         input_string = '(;A[b]C[d])'

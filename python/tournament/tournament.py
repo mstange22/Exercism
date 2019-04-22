88b119b88afd0dade228e5087fbc978a773349b1
Mike S <mstange@dthera.com>
2019-04-22 10:32:59 -0700

    revised tournament solution

--- python/tournament/tournament.py
+++ python/tournament/tournament.py
@@ -1,20 +1,17 @@
+from collections import defaultdict
+
 def tally(tournament_results):
-    res = ['Team                           | MP |  W |  D |  L |  P']
-    if len(tournament_results) > 0:
+    formatted_string = '{:<31}|{:>3} |{:>3} |{:>3} |{:>3} |{:>3}'
+    res = [formatted_string.format('Team', 'MP', 'W', 'D', 'L', 'P')]
+    if tournament_results:
         teams = {}
-        for line in tournament_results.split('\n'):
+        for line in tournament_results.splitlines():
             game_data = line.split(';')
 
             for i in range(2):
                 # add team to dict if not yet seen
                 if (game_data[i] not in teams.keys()):
-                    teams[game_data[i]] = {
-                        'matches': 0,
-                        'wins': 0,
-                        'draws': 0,
-                        'losses': 0,
-                        'points': 0,
-                    }
+                    teams[game_data[i]] = defaultdict(int)
                 teams[game_data[i]]['matches'] += 1
 
             team1 = game_data[0]
@@ -36,7 +33,6 @@ def tally(tournament_results):
 
         # sort by points (desc), then by team name
         for team in sorted(teams.keys(), key=lambda x: (-teams[x]['points'], x)):
-            res.append(f'{team:<31}|{teams[team]["matches"]:>3} |'
-                       f'{teams[team]["wins"]:>3} |{teams[team]["draws"]:>3} |'
-                       f'{teams[team]["losses"]:>3} |{teams[team]["points"]:>3}')
+            res.append(formatted_string.format(team, teams[team]['matches'], teams[team]['wins'],
+                teams[team]['draws'], teams[team]['losses'], teams[team]['points']))
     return '\n'.join(res)

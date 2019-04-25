486e858e76d3dab2c41054119fab9b8007a31de1
Mike S <mstange@dthera.com>
2019-04-25 12:03:14 -0700

    revised clock solution

--- c/clock/src/clock.c
+++ c/clock/src/clock.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 #define MINUTES_IN_DAY 1440
+#define FORMAT "%02i:%02i"
 
 void clock(time_text_t time_text, int hour, int minute)
 {
+  if (!time_text) return;
   // use total minutes
   minute += (hour * 60);
   // truncate / shift to 24-hour range
@@ -14,14 +16,15 @@ void clock(time_text_t time_text, int hour, int minute)
 		minute += MINUTES_IN_DAY;
 	}
   // write hours and minutes to time_text using div / mod
-  sprintf(time_text, "%02i:%02i", minute / 60, minute % 60);
+  snprintf(time_text, MAX_STR_LEN, FORMAT, minute / 60, minute % 60);
 }
 
 void clock_add(time_text_t time_text, int minute_offset)
 {
+  if (!time_text) return;
+  int hour, minute;
   // extrapolate hour and minute from time_text
-  int hour = ((time_text[0] - '0') * 10) + (time_text[1] - '0');
-  int minute = ((time_text[3] - '0') * 10) + (time_text[4] - '0');
+  sscanf(time_text, FORMAT, &hour, &minute);
   // use clock function with new minute value
   clock(time_text, hour, minute + minute_offset);
 }

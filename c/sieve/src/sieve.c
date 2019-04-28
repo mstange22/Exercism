6e8b5672cfef1de8d0e6f5e1c993cca876c663ef
Mike S <mstange@dthera.com>
2019-04-28 08:07:50 -0700

    sieve solution

---
+++ c/sieve/src/sieve.c
@@ -0,0 +1,24 @@
+#include "sieve.h"
+#include <math.h>
+
+unsigned int sieve(const unsigned int limit, primes_array_t primes)
+{
+  int prime_count = 0;
+  int marked_array[MAX_LIMIT_TESTED + 1];
+  // initialize marked_array
+  for (int i = 0; i <= MAX_LIMIT_TESTED; i++)
+  {
+    marked_array[i] = 0;
+  }
+  for (unsigned int i = 2; i <= limit; i++) {
+    if (!marked_array[i])
+    {
+      primes[prime_count++] = i;
+      for (unsigned int j = i; j <= limit; j += i)
+      {
+        marked_array[j] = 1;
+      }
+    }
+  }
+  return prime_count;
+}

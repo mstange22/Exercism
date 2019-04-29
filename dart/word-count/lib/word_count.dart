b05182c1794ae6f5a1ef005eb12ec0f975e2d0c4
Mike S <mstange@dthera.com>
2019-04-29 12:55:46 -0700

    revised word_count solution

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,18 +1,13 @@
 class WordCount {
     Map<String, int> countWords(String s) {
       Map<String, int> res = new Map<String, int>();
-      List<String> words = s.split(new RegExp(r'[\s.,:!&@$%^&]+'));
-      for (int i = 0; i < words.length; i++) {
-        var word = words[i].toLowerCase();
+      List<String> words = s.toLowerCase().split(new RegExp(r'[\s.,:!&@$%^&]+'));
+      for (var word in words) {
         if (word.length >= 1) {
           if (word[0] == '\'' && word[word.length -1] == '\'') {
             word = word.substring(1, word.length - 1);
           }
-          if (res.containsKey(word)) {
-            res[word]++;
-          } else {
-            res[word] = 1;
-          }
+          res.containsKey(word) ? res[word]++ : res[word] = 1;
         }
       }
       return res;

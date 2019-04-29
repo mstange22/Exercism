16549085bd7dbdecb1830d7968a3b686787992c2
Mike S <mstange@dthera.com>
2019-04-29 13:34:59 -0700

    bob solution

---
+++ dart/bob/lib/bob.dart
@@ -0,0 +1,22 @@
+class Bob {
+  String response(String input) {
+    input = input.trim();
+    if (input == '') {
+      return 'Fine. Be that way!';
+    }
+    if (input.endsWith('?')) {
+      if (isYelling(input)) {
+        return 'Calm down, I know what I\'m doing!';
+      }
+      return 'Sure.';
+    }
+    if (isYelling(input)) {
+      return 'Whoa, chill out!';
+    }
+    return 'Whatever.';
+  }
+}
+
+bool isYelling(String s) {
+  return (s.toUpperCase() == s && s.contains(new RegExp(r'[A-Z]')));
+}

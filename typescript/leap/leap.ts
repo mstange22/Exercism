0e7a1f533038d0782ff9685325d7a39eca9c6015
Mike S <mstange@dthera.com>
2019-05-01 08:41:42 -0700

    revised leap solution

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,1 +1,6 @@
-export default (year: number) => year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+export default (year: number) => {
+  if (year % 4 === 0) {
+    return year % 100 !== 0 || year % 400 === 0;
+  }
+  return false;
+};

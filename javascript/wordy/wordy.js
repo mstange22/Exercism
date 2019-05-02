3bf03f1019a918faf0c7a2393779dc56f68cc2b4
Mike S <mstange@dthera.com>
2019-05-02 11:09:21 -0700

    refactored wordy solution

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -2,34 +2,7 @@ export class ArgumentError extends Error {}
 
 export class WordProblem {
   constructor(question) {
-    this._answer = this.evaluate(question.slice(0, -1).replace(/ by/g, '').split(' ').slice(2));
-  }
-
-  evaluate([operand1, operator, operand2, ...remainingQuestion]) {
-    operand1 = Number(operand1);
-    operand2 = Number(operand2);
-    let res;
-
-    switch (operator) {
-      case 'plus':
-        res = operand1 + operand2;
-        break;
-      case 'minus':
-        res = operand1 - operand2;
-        break;
-      case 'multiplied':
-        res = operand1 * operand2;
-        break;
-      case 'divided':
-        res = operand1 / operand2;
-        break;
-      default:
-        return null;
-    }
-    if (remainingQuestion.length !== 0) {
-      return this.evaluate([res, ...remainingQuestion]);
-    }
-    return res;
+    this._answer = evaluate(question.slice(0, -1).replace(/ by/g, '').split(' ').slice(2));
   }
 
   answer() {
@@ -37,3 +10,20 @@ export class WordProblem {
     return this._answer;
   }
 }
+
+const evaluate = ([operand1, operator, operand2, ...remainingQuestion]) => {
+  let res = performOperation(Number(operand1), operator, Number(operand2));
+
+  if (remainingQuestion.length !== 0) {
+    return evaluate([res, ...remainingQuestion]);
+  }
+  return res;
+};
+
+const performOperation = (op1, operator, op2) => {
+  if (operator === 'plus') return op1 + op2;
+  if (operator === 'minus') return op1 - op2;
+  if (operator === 'multiplied') return op1 * op2;
+  if (operator === 'divided') return op1 / op2;
+  return null;
+};

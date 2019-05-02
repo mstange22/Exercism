11dfb2000b178e7434e64919ede98d4231e04f71
Mike S <mstange@dthera.com>
2019-05-02 07:32:41 -0700

    added sxm and qw()

--- perl5/bob/Bob.pm
+++ perl5/bob/Bob.pm
@@ -7,17 +7,17 @@ our @EXPORT_OK = qw(hey);
 
 sub hey {
   my ($msg) = @_;
-  $msg =~ s/^\s+|\s+$//g;
+  $msg =~ s/^\s+|\s+$//gsxm;
   if ($msg eq '') {
     return 'Fine. Be that way!';
   }
-  if ((substr $msg, -1) eq '?') {
-    if (uc($msg) eq $msg && $msg =~ /[A-Z]/) {
+  if ((substr $msg, -1) eq qw(?)) {
+    if (uc($msg) eq $msg && $msg =~ /[A-Z]/sxm) {
       return 'Calm down, I know what I\'m doing!';
     }
     return 'Sure.'
   }
-  if (uc($msg) eq $msg && $msg =~ /[A-Z]/) {
+  if (uc($msg) eq $msg && $msg =~ /[A-Z]/sxm) {
     return 'Whoa, chill out!';
   }
   return 'Whatever.'

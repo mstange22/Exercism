810cb28ce13ab7a2046adbe050546fd0911b7588
Mike S <mstange@dthera.com>
2019-05-02 15:45:30 -0700

    added param validation

--- perl5/grains/Grains.pm
+++ perl5/grains/Grains.pm
@@ -1,11 +1,15 @@
 package Grains;
 use strict;
 use warnings;
+use Carp;
 use Exporter 'import';
 our @EXPORT_OK = qw(grains_on_square total_grains);
 
 sub grains_on_square {
   my ($square) = @_;
+  if ($square < 1 || $square > 64) {
+    throw Exception('square must be between 1 and 64');
+  }
   return 1 << ($square - 1);
 }
 

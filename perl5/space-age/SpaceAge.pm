9c37c945fd6dddb46444e169ccb2d2943f6a4dce
Mike S <mstange@dthera.com>
2019-05-04 14:54:24 -0700

    revised space-age solution

--- perl5/space-age/SpaceAge.pm
+++ perl5/space-age/SpaceAge.pm
@@ -1,57 +1,50 @@
-package SpaceAge;
-use strict;
-use warnings;
-use Exporter 'import';
-our @EXPORT_OK = qw(new);
+package SpaceAge {
+  use strict;
+  use warnings;
 
-sub new {
-  my ($class, $seconds) = @_;
-  return bless {
-      seconds => $seconds,
-  }, $class;
-}
+  sub new {
+    my ($class, $seconds) = @_;
+    return bless {
+        seconds => $seconds,
+        earth_years => $seconds / 31557600,
+    }, $class;
+  }
 
-sub seconds {
-  my $self = shift;
-  return $self->{seconds}
-}
+  sub seconds {
+    return shift->{seconds}
+  }
 
-sub on_earth {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600);
-}
+  sub on_earth {
+    return sprintf("%.2f", shift->{earth_years});
+  }
 
-sub on_mercury {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600 / 0.2408467);
-}
+  sub on_mercury {
+    return sprintf("%.2f", shift->{earth_years}/ 0.2408467);
+  }
 
-sub on_venus {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600 / 0.61519726);
-}
+  sub on_venus {
+    return sprintf("%.2f", shift->{earth_years} / 0.61519726);
+  }
 
-sub on_mars {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600 / 1.8808158);
-}
+  sub on_mars {
+    return sprintf("%.2f", shift->{earth_years}/ 1.8808158);
+  }
 
-sub on_jupiter {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600 / 11.862615);
-}
+  sub on_jupiter {
+    return sprintf("%.2f", shift->{earth_years} / 11.862615);
+  }
 
-sub on_saturn {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600 / 29.447498);
-}
+  sub on_saturn {
+    return sprintf("%.2f", shift->{earth_years} / 29.447498);
+  }
 
-sub on_uranus {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600 / 84.016846 );
-}
+  sub on_uranus {
+    return sprintf("%.2f", shift->{earth_years} / 84.016846 );
+  }
 
-sub on_neptune {
-  my $self = shift;
-  return sprintf("%.2f", $self->{seconds} / 31557600 / 164.79132);
-}
+  sub on_neptune {
+    return sprintf("%.2f", shift->{earth_years} / 164.79132);
+  }
+};
+
+1;

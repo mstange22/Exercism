ae4fa823dcf5aac13485fe74dc7d195cc8decb85
Mike S <mstange@dthera.com>
2019-05-07 08:01:12 -0700

    revised scrabble-score solution

--- perl5/scrabble-score/ScrabbleScore.pm
+++ perl5/scrabble-score/ScrabbleScore.pm
@@ -4,17 +4,16 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(score);
 
-my %given_map = (
-  1 => "AEIOULNSRT",
-  2 => "DG",
-  3 => "BCMP",
-  4 => "FHVWY",
-  5 => "K",
-  8 => "JX",
-  10 => "QZ",
-);
-
 sub build_map {
+  my %given_map = (
+    1 => "AEIOULNSRT",
+    2 => "DG",
+    3 => "BCMP",
+    4 => "FHVWY",
+    5 => "K",
+    8 => "JX",
+    10 => "QZ",
+  );
   my %new_map = ();
   foreach my $key (keys %given_map) {
     foreach my $letter (split('', $given_map{$key})) {
@@ -28,9 +27,8 @@ sub score {
   my ($word, %extensions) = @_;
   my $map = build_map();
   my $points = 0;
-  foreach(split('', $word)) {
-    my $curr_points = $map->{uc($_)};
-    $points += $curr_points;
+  for my $letter (split('', uc($word))) {
+    $points += $map->{$letter};
   }
   return $points;
 }

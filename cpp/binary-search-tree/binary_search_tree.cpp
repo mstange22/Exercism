ce3575e57848e9590630ca68eca432fe247c0213
Mike S <mstange@dthera.com>
2019-05-07 20:11:32 -0700

    binary_search_tree progress

---
+++ cpp/binary-search-tree/binary_search_tree.cpp
@@ -0,0 +1,11 @@
+#include "binary_search_tree.h"
+
+template<typename T>
+binary_tree::binary_tree<T>::binary_tree() {
+
+}
+
+template<typename T>
+binary_tree::binary_tree<T>::binary_tree(const std::vector<T> *data_iter) {
+
+}
---
+++ cpp/binary-search-tree/binary_search_tree.h
@@ -0,0 +1,13 @@
+#ifndef BINARY_SEARCH_TREE_H
+# define BINARY_SEARCH_TREE_H
+
+namespace binary_tree {
+  template <class T>
+  class binary_tree {
+    binary_tree();
+    binary_tree(const std::vector<T> *data);
+    public:
+  };
+}
+
+#endif

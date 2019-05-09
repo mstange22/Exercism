591eaa2c1407ff3aa6b5a7b2e0e894a828d103fd
Mike S <mstange@dthera.com>
2019-05-08 19:49:15 -0700

    removed print statements

--- perl5/binary-search/BinarySearch.pm
+++ perl5/binary-search/BinarySearch.pm
@@ -6,21 +6,16 @@ our @EXPORT_OK = qw(binary_search);
 
 sub binary_search {
   my ($input) = @_;
-  print "$input\n";
   my $arr = $input->{"array"};
   my $target = $input->{"value"};
-  print "arr: $arr\n";
-  print "value: $target\n";
 
   my $min = 0;
   my $max = scalar(@{$arr}) - 1;
-  print "max: $max\n";
 
   while ($min <= $max) {
     use integer;
     my $mid = ($min + $max) / 2;
     my $mid_val = $arr->[$mid];
-    print "mid_val: $mid_val\n";
     if ($mid_val == $target) {
       return $mid;
     } elsif ($mid_val < $target) {

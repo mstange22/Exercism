f36048c89ec106bad62c5f1851a3502a91b8a5cb
Mike S <mstange@dthera.com>
2019-05-28 22:05:01 -0700

    revised flatten-array solution

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -3,30 +3,26 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 
-@SuppressWarnings("unchecked")
 class Flattener {
   List<Object> flatten(List<Object> list) {
     List<Object> res = new ArrayList<Object>();
 
-    // convert to deque for removing and adding to front of list
+    // convert list to deque for removing and adding to front of list
     Deque<Object> deque = new LinkedList<>(list);
 
     while (!deque.isEmpty()) {
-
-      // get first element
       Object first = deque.removeFirst();
+      if (first == null) {
+        continue;
+      }
 
-      // if first is a single element, add it to res
-      if (!(first instanceof List) && first != null) {
-        res.add(first);
-      } else if (first != null) {
-        // we have a list, so convert to deque
-        Deque<Object> firstDeque = new LinkedList<Object>((List<Object>)first);
-
-        // push each element back onto front of deque
+      if (first instanceof List) {
+        Deque<Object> firstDeque = new LinkedList<Object>((List<?>)first);
         while(!firstDeque.isEmpty()) {
           deque.addFirst(firstDeque.removeLast());
         }
+      } else {
+        res.add(first);
       }
     }
     return res;

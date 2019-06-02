c2f8c55e7b106d7cf64792f906b6c8bee9ec6cbd
Mike S <mstange@dthera.com>
2019-06-02 12:38:13 -0700

    revised robot-name solution

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -1,11 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Collections;
 
 class Robot {
-
+  static List<String> robotNames = new ArrayList<String>();
+  static int currentNameIndex = 0;
   private String name;
 
+  static {
+    generateAllPossibleRobotNames();
+    shuffleRobotNames();
+  }
+
   Robot() {
-    reset();
+    generateNewRobotName();
   }
 
   String getName() {
@@ -13,13 +22,39 @@ class Robot {
   }
 
   void reset() {
-    Random r = new Random();
-    StringBuilder newName = new StringBuilder();
-    newName.append((char)(r.nextInt(26) + (int)'A'));
-    newName.append((char)(r.nextInt(26) + (int)'A'));
-    for (int i = 0; i < 3; i++) {
-      newName.append(r.nextInt(10));
+    generateNewRobotName();
+  }
+
+  void generateNewRobotName() {
+    name = robotNames.get(currentNameIndex++);
+  }
+
+  static void generateAllPossibleRobotNames() {
+    for (int a = 0; a < 26; a++) {
+      for (int b = 0; b < 26; b++) {
+        for (int i = 0; i < 10; i++) {
+          for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 10; k++) {
+              StringBuilder newName = new StringBuilder();
+              newName.append((char)(a + (int)'A'));
+              newName.append((char)(b + (int)'A'));
+              newName.append(i);
+              newName.append(j);
+              newName.append(k);
+              robotNames.add(newName.toString());
+            }
+          }
+        }
+      }
     }
-    name = newName.toString();
+  }
+
+  static void shuffleRobotNames() {
+    Random r = new Random();
+    int lastElementIndex = robotNames.size() - 1;
+    for (int i = 0; i <= lastElementIndex; i++) {
+      int j = r.nextInt(lastElementIndex - i + 1) + i;
+      Collections.swap(robotNames, i, j);
+    }
   }
 }

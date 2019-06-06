0f84155cfc9058a7c374cbf4b74e3d3d1b662023
Mike S <mstange@dthera.com>
2019-06-06 08:20:21 -0700

    revised binary-search solution

--- java/binary-search/src/main/java/BinarySearch.java
+++ java/binary-search/src/main/java/BinarySearch.java
@@ -3,25 +3,23 @@ import java.util.List;
 class BinarySearch {
   private List<Integer> list;
 
-  BinarySearch(List<Integer> givenList) {
-    list = givenList;
+  BinarySearch(List<Integer> list) {
+    this.list = list;
   }
 
   int indexOf(int target) throws ValueNotFoundException {
     int min = 0;
     int max = list.size() - 1;
-    if (list.size() != 0) {
-      while (min <= max) {
-        int mid = (min + max) / 2;
-        int midValue = list.get(mid);
-        if (midValue == target) {
-          return mid;
-        }
-        if (target < midValue) {
-          max = mid - 1;
-        } else {
-          min = mid + 1;
-        }
+    while (min <= max) {
+      int mid = (min + max) / 2;
+      int midValue = list.get(mid);
+      if (midValue == target) {
+        return mid;
+      }
+      if (target < midValue) {
+        max = mid - 1;
+      } else {
+        min = mid + 1;
       }
     }
     throw new ValueNotFoundException("Value not in array");

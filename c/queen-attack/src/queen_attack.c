80b08b555404e399e664deca281beef397e4c168
Mike S <mstange@dthera.com>
2019-07-24 05:41:36 -0700

    removed check for negative position input

--- c/queen-attack/src/queen_attack.c
+++ c/queen-attack/src/queen_attack.c
@@ -4,8 +4,8 @@
 attack_status_t can_attack(position_t queen_1, position_t queen_2)
 {
   // validate positions
-  if (queen_1.column < 0 || queen_1.column > 7 || queen_1.row < 0 || queen_1.row > 7 ||
-    queen_2.column < 0 || queen_2.column > 7 || queen_2.row < 0 || queen_2.row > 7 ||
+  if (queen_1.column > 7 || queen_1.row > 7 ||
+    queen_2.column > 7 || queen_2.row > 7 ||
     // queens on same position
     (queen_1.column == queen_2.column && queen_1.row == queen_2.row))
   {

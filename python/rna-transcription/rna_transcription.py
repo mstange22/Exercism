06111b04df56ac9dcdeec308b2419b99bfabae2f
Mike S <mstange@dthera.com>
2019-09-02 08:50:24 -0700

    revised rna_transcription solution

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,13 +1,8 @@
-def to_rna(dna_strand):
-  rna_strand = ''
-  for char in dna_strand:
-    if char == 'G':
-      rna_strand += 'C'
-    elif char == 'C':
-      rna_strand += 'G'
-    elif char == 'T':
-      rna_strand += 'A'
-    else:
-      rna_strand += 'U'
-  
-  return rna_strand
+def to_rna(s):
+  m = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+  }
+  return ''.join([m[c] for c in s])

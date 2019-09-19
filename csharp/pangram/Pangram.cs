6bafa9332c574a2443ab3914a1173c5e624aecf0
Mike S <mstange@dthera.com>
2019-09-19 14:45:58 -0700

    pangram solution

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Pangram
+{
+    private static string alpha = "abcdefghijklmnopqrstuvwxyz";
+    public static bool IsPangram(string input)
+    {
+        string lower = input.ToLower();
+        foreach (char c in alpha) {
+            if (lower.IndexOf(c) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

05e0ca868dcd55d6090c1576461e2416ec502b13
Mike S <mstange@dthera.com>
2019-09-19 14:07:15 -0700

    collatz-conjecture solution

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        int steps = 0;
+        if (number <= 0) throw new ArgumentException("Number must be greater than zero.");
+        while (number > 1) {
+            if (number % 2 == 0) {
+                number /= 2;
+            } else {
+                number = (number * 3) + 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}

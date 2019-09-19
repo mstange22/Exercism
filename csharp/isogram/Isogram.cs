7f1a6b092b8fc189a21c6f97110c91e1cf478a0e
Mike S <mstange@dthera.com>
2019-09-19 15:33:47 -0700

    isogram solution

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        HashSet<char> letters = new HashSet<char>();
+        string normalized = new Regex("[- ]").Replace(word, "").ToLower();
+        foreach(char c in normalized) {
+            if (letters.Contains(c)) {
+                return false;
+            } else {
+                letters.Add(c);
+            }
+        }
+        return true;
+    }
+}

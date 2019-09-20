fca1fbe75ff088c17e873e5d6dba0eb67ca9e3c1
Mike S <mstange@dthera.com>
2019-09-20 05:51:24 -0700

    reverse-string solution

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        StringBuilder res = new StringBuilder();
+        for (int i = input.Length - 1; i >= 0; i--) {
+            res.Append(input[i]);
+        }
+        return res.ToString();
+    }
+}

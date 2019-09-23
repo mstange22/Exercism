95ba9913778f5d6f875b63074dd03fbf140c8243
Mike S <mstange@dthera.com>
2019-09-23 07:07:30 -0700

    sum-of-multiples solution

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> nums, int max)
+    {
+        int sum = 0;
+        HashSet<int> multiples = new HashSet<int>();
+        foreach (int n in nums) {
+            for (int i = 0; n != 0 && i * n < max; i++) {
+                if (!multiples.Contains(i * n)) {
+                    sum += (i * n);
+                    multiples.Add(i * n);
+                }
+            }
+        }
+        return sum;
+    }
+}

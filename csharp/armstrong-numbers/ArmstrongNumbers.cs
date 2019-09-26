a34d1252423f8efd0aea08985367029f84c889aa
Mike S <mstange@dthera.com>
2019-09-26 12:20:34 -0700

    armstrong-numbers solution

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string numberString = number.ToString();
+        return (int)numberString.Sum(c => Math.Pow(Char.GetNumericValue(c), numberString.Length)) == number;
+    }
+}

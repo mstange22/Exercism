acc74b4bfd36b300410457025e639214be1510bc
Mike S <mstange@dthera.com>
2019-09-26 12:20:47 -0700

    hamming solution

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,18 @@
+using System;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        int distance = 0;
+        if (firstStrand.Length != secondStrand.Length) {
+            throw new ArgumentException("Strands must be the same length.");
+        }
+        for (int i = 0; i < firstStrand.Length; i++) {
+            if (firstStrand[i] != secondStrand[i]) {
+                distance++;
+            }
+        }
+        return distance;
+    }
+}

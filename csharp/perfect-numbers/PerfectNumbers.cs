f2684d91cc326de81848acbb9cdce36eb9e8f047
Mike S <mstange@dthera.com>
2019-09-27 14:27:10 -0700

    perfect-numbers solution

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        int sum = 1;
+        if (number <= 0) {
+            throw new ArgumentOutOfRangeException("Number must be greater than 0.");
+        }
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number && number != 1 ? Classification.Perfect :
+            sum > number ? Classification.Abundant : Classification.Deficient;
+    }
+}

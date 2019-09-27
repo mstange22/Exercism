00cec7a884471a12be48088ef316b229cb7fc8cb
Mike S <mstange@dthera.com>
2019-09-27 14:25:36 -0700

    revised solution using Linq

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,18 +1,15 @@
 using System;
+using System.Linq;
+
 
 public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        int distance = 0;
         if (firstStrand.Length != secondStrand.Length) {
             throw new ArgumentException("Strands must be the same length.");
         }
-        for (int i = 0; i < firstStrand.Length; i++) {
-            if (firstStrand[i] != secondStrand[i]) {
-                distance++;
-            }
-        }
-        return distance;
+        // return firstStrand.Select((c, i) => c == secondStrand[i] ? 0 : 1).Sum();
+        return firstStrand.Zip(secondStrand, (first, second) => first == second ? 0 : 1).Sum();
     }
 }

56f6cbd9b376ae5ac376955634976250c8002c44
Mike S <mstange@dthera.com>
2019-09-28 14:34:33 -0700

    proverb solution

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        var res = new List<string>();
+
+        if (subjects.Length > 0) {
+            for (int i = subjects.Length - 1; i >= 0; i--) {
+                if (i > 0) {
+                    res.Insert(0, $"For want of a {subjects[i - 1]} the {subjects[i]} was lost.");
+                } else {
+                    res.Add($"And all for the want of a {subjects[i]}.");
+                }
+            }
+        }
+        return res.ToArray();
+    }
+}

e95c07ec7508d6cf2aa5935b9b570fc4b0473546
Mike S <mstange@dthera.com>
2019-09-28 11:22:26 -0700

    darts solution

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double z = Math.Sqrt(x * x + y * y);
+        if (z > 10) return 0;
+        if (z > 5) return 1;
+        if (z > 1) return 5;
+        return 10;
+    }
+}

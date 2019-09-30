5720e6fb0c42feb7c5706900e33a27f0cd05c626
Mike S <mstange@dthera.com>
2019-09-30 15:50:43 -0700

    parallel-letter-frequency solution

---
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ParallelLetterFrequency
+{
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+    {
+        var queryResult = texts
+            .AsParallel()
+            .SelectMany(text => text
+                .ToLower()
+                .Where(char.IsLetter)
+                .GroupBy(letter => letter)
+            );
+
+        var res = new Dictionary<char, int>();
+
+        foreach (var letter in queryResult) {
+            if (!res.ContainsKey(letter.Key)) {
+                res[letter.Key] = letter.Count();
+            } else {
+                res[letter.Key] += letter.Count();
+            }
+        }
+        return res;
+    }
+}

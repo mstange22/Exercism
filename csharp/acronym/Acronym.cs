08b223ed91464f5d2910a8e1c3a400ac097fa04a
Mike S <mstange@dthera.com>
2019-09-30 10:10:32 -0700

    acronym solution

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string[] separator = { " ", "-", "_" };
+        string[] tokens = phrase.ToUpper().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        return string.Join("", tokens.Select(token => token[0]));
+    }
+}

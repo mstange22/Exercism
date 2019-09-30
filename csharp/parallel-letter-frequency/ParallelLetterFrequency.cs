ec23dac6fb1d3ada7d8db5986d155ef0f11bbeb5
Mike S <mstange@dthera.com>
2019-09-30 16:00:34 -0700

    better variable names

--- csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -6,21 +6,21 @@ public static class ParallelLetterFrequency
 {
     public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
     {
-        var queryResult = texts
+        var groups = texts
             .AsParallel()
             .SelectMany(text => text
                 .ToLower()
-                .Where(char.IsLetter)
+                .Where(Char.IsLetter)
                 .GroupBy(letter => letter)
             );
 
         var res = new Dictionary<char, int>();
 
-        foreach (var letter in queryResult) {
-            if (!res.ContainsKey(letter.Key)) {
-                res[letter.Key] = letter.Count();
+        foreach (var group in groups) {
+            if (!res.ContainsKey(group.Key)) {
+                res[group.Key] = group.Count();
             } else {
-                res[letter.Key] += letter.Count();
+                res[group.Key] += group.Count();
             }
         }
         return res;

46e9df40afb213bb23a2403b40d4e264849aef75
Mike S <mstange@dthera.com>
2019-10-01 15:49:35 -0700

    pascals-triangle solution

---
+++ csharp/pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        var res = new List<List<int>>();
+
+        if (rows == 0) {
+            return res;
+        }
+
+        res.Add(new List<int>(){ 1 });
+
+        for (int i = 1; i < rows; i++) {
+            var row = new List<int>();
+
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    row.Add(res[i - 1][0]);
+                } else if (j == i)  {
+                    row.Add(res[i - 1][res[i - 1].Count - 1]);
+                } else {
+                    row.Add(res[i - 1][j - 1] + res[i - 1][j]);
+                }
+            }
+            res.Add(row);
+        }
+        return res;
+    }
+}

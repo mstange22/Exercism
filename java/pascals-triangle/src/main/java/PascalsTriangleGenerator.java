5f154775ef9be7d842852e23feb349c85f8b8934
Mike S <mstange@dthera.com>
2019-10-10 14:52:49 -0700

    pascals-triangle solution

---
+++ java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class PascalsTriangleGenerator {
+
+  int[][] generateTriangle(int rows) {
+    int[][] res = new int[rows][];
+    
+    for (int i = 0; i < rows; i++) {
+      int[] row = new int[i + 1];
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          row[j] = 1;
+        } else {
+          row[j] = res[i - 1][j - 1] + res[i - 1][j];
+        }
+      }
+      res[i] = row;
+    }
+    return res;
+  }
+
+}

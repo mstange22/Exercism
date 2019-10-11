9fde727706d6804d72110808ed1a12645777d3c5
Mike S <mstange@dthera.com>
2019-10-11 15:20:16 -0700

    kindergarten garden solution

---
+++ csharp/kindergarten-garden/KindergartenGarden.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Plant
+{
+    Violets,
+    Radishes,
+    Clover,
+    Grass
+}
+
+public class KindergartenGarden
+{    
+    private string[] garden;
+    private static Dictionary<char, Plant> map = new Dictionary<char, Plant>(){
+        { 'V', Plant.Violets },
+        { 'R', Plant.Radishes },
+        { 'C', Plant.Clover },
+        { 'G', Plant.Grass },
+    };
+    public KindergartenGarden(string diagram)
+    {
+        garden = diagram.Split(new []{ '\n' });
+    }
+
+    public IEnumerable<Plant> Plants(string student)
+    {
+        return new Plant[]{
+            map[garden[0][(student[0] - 'A') * 2]],
+            map[garden[0][((student[0] - 'A') * 2) + 1]],
+            map[garden[1][(student[0] - 'A') * 2]],
+            map[garden[1][((student[0] - 'A') * 2) + 1]],
+        };
+    }
+}

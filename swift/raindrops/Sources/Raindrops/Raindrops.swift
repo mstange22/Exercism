612733347d08b4c3a44abfba9f81af6295361264
Mike S <mstange@dthera.com>
2019-10-14 14:47:18 -0700

    raindrops solution

---
+++ swift/raindrops/Sources/Raindrops/Raindrops.swift
@@ -0,0 +1,14 @@
+struct Raindrops {
+  static var soundMap = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+  var sounds = ""
+  init(_ n: Int) {
+    for (num, sound) in Raindrops.soundMap {
+      if n % num == 0 {
+        self.sounds += sound
+      }
+    } 
+    if self.sounds == "" {
+      self.sounds = String(n)
+    }
+  }
+}

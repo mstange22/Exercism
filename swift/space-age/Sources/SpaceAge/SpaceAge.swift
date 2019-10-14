713e03f70836a3285090eea25f030ca1754ceac6
Mike S <mstange@dthera.com>
2019-10-14 12:12:59 -0700

    space-age solution

---
+++ swift/space-age/Sources/SpaceAge/SpaceAge.swift
@@ -0,0 +1,22 @@
+class SpaceAge {
+  var seconds: Int
+  var onEarth: Double
+  var onMercury: Double
+  var onVenus: Double
+  var onMars: Double
+  var onJupiter: Double
+  var onSaturn: Double
+  var onUranus: Double
+  var onNeptune: Double
+  init(_ age: Int) {
+    seconds = age
+    onEarth = Double(seconds) / 31557600
+    onMercury = onEarth / 0.2408467
+    onVenus = onEarth / 0.61519726
+    onMars = onEarth / 1.8808158
+    onJupiter = onEarth / 11.862615
+    onSaturn = onEarth / 29.447498
+    onUranus = onEarth / 84.016846
+    onNeptune = onEarth / 164.79132
+  }
+}

dd155470db3395d02649e083daf9e91c5b5a6e1b
Mike S <mstange@dthera.com>
2019-10-21 10:30:37 -0700

    sieve solution

--- swift/sieve/Sources/Sieve/Sieve.swift
+++ swift/sieve/Sources/Sieve/Sieve.swift
@@ -1,1 +1,17 @@
-//Solution goes in Sources
+class Sieve {
+    var primes = [Int]()
+    init(_ n: Int) {
+        var composites = Set<Int>()
+        for i in 2...n {
+            if (!composites.contains(i)) {
+                primes.append(i)
+                // mark multiples of i as composite
+                var j = i * i
+                while j <= n {
+                    composites.insert(j)
+                    j += i
+                }
+            }
+        }
+    }
+}

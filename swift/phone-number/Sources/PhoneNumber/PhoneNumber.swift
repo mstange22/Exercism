02976926688671cb05eb6686c936bf79a9e93449
Mike S <mstange@dthera.com>
2019-10-21 12:06:51 -0700

    revised phone-number solution

--- swift/phone-number/Sources/PhoneNumber/PhoneNumber.swift
+++ swift/phone-number/Sources/PhoneNumber/PhoneNumber.swift
@@ -3,8 +3,6 @@ import Foundation
 class PhoneNumber {
     var number = "0000000000"
     var areaCode: String { String(number.prefix(3)) }
-    var exchange : String { String(number[number.index(number.startIndex, offsetBy: 3)...number.index(number.startIndex, offsetBy: 5)]) }
-    var suffix : String { String(number.suffix(4)) }
 
     init(_ startingNumber: String) {
         let strippedNumber = startingNumber.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression);
@@ -12,15 +10,16 @@ class PhoneNumber {
             self.number = strippedNumber
         } else if strippedNumber.count == 11 {
             if strippedNumber.hasPrefix("1") {
-                let index = strippedNumber.index(strippedNumber.startIndex, offsetBy: 1)
-                self.number = String(strippedNumber[index...])
+                self.number = String(strippedNumber.suffix(10))
             }
         }
     }
 }
 
-extension PhoneNumber: CustomStringConvertible {
-    var description: String {
-        return "(\(areaCode)) \(exchange)-\(suffix)"
+extension String {
+    init(describing: PhoneNumber) {
+        let number = describing.number
+        let exchange = String(number[number.index(number.startIndex, offsetBy: 3)...number.index(number.startIndex, offsetBy: 5)])
+        self = "(\(describing.areaCode)) \(exchange)-\(String(number.suffix(4)))"
     }
 }

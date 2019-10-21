b724f120cf06fb06128b986bbc023cc7b4221ace
Mike S <mstange@dthera.com>
2019-10-21 15:42:20 -0700

    rename variable

--- swift/collatz-conjecture/Sources/CollatzConjecture/CollatzConjecture.swift
+++ swift/collatz-conjecture/Sources/CollatzConjecture/CollatzConjecture.swift
@@ -2,12 +2,12 @@ enum CollatzError: Error {
     case invalidInput(String)
 }
 
-func steps(_ input: Int) throws -> Int {
-    if input < 1 {
+func steps(_ n: Int) throws -> Int {
+    var n = n
+    if n < 1 {
         throw CollatzError.invalidInput("Input must be greater than 0.")
     }
     var count = 0
-    var n = input
     while n > 1 {
         if (n % 2 == 0) {
             n /= 2

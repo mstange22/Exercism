4476aa7c76ef7b22cb1e04b7a011ffa46cae8270
Mike S <mstange@dthera.com>
2019-10-22 12:13:50 -0700

    revised pangram solution

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,13 +1,9 @@
-const SHIFT = 'a'.charCodeAt();
-
-export const isPangram = (incomingString) => {
-  return incomingString
-    .toLowerCase()
-    .replace(/[^a-z]/, '')
-    .split('')
-    .reduce((map, char) => {
-      map[char.charCodeAt() - SHIFT] = true;
-      return map;
-    }, new Array(26).fill(false))
-    .every(el => el);
-};
+export const isPangram = (s) => {
+  const seen = new Set();
+  for (const c of s.toLowerCase()) {
+    if (/[a-z]/.test(c) && !seen.has(c)) {
+      seen.add(c);
+    }
+  }
+  return seen.size === 26;
+}

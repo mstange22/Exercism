7480796a94e7de6556177556652d1d2405908e0d
Mike S <mstange@dthera.com>
2019-10-22 15:49:21 -0700

    implicit return

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,7 +1,5 @@
 package Acronym
 
-fun generate(phrase: String): String {
-  return phrase.toUpperCase()
-    .split('-', ' ')
-    .fold("") { acc, e -> if (e.length > 0) acc + e[0] else acc }
-}
+fun generate(phrase: String): String = phrase
+  .toUpperCase().split('-', ' ')
+  .fold("") { acc, e -> if (e.length > 0) acc + e[0] else acc }

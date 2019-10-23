5637a133d91c1ee390aab1a5b767d3df0f7b7db1
Mike S <mstange@dthera.com>
2019-10-23 06:54:30 -0700

    revised hamming solution

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,8 +1,6 @@
 package Hamming
 
 fun compute(strand1: String, strand2: String): Int {
-  if (strand1.length != strand2.length) {
-    throw IllegalArgumentException("left and right strands must be of equal length.")
-  }
-  return strand1.zip(strand2).fold(0) { acc, (s1, s2) -> if (s1 != s2) acc + 1 else acc }
+  require(strand1.length == strand2.length) { "left and right strands must be of equal length." }
+  return strand1.zip(strand2).count { (s1, s2) -> (s1 != s2) }
 }

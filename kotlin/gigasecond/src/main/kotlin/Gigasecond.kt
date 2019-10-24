0a8a4a0158079d0e61779467b48070f759692923
Mike S <mstange@dthera.com>
2019-10-24 08:52:30 -0700

    use scientific notation

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 class Gigasecond(moment: LocalDateTime) {
-  var date: LocalDateTime = moment.plusSeconds(1000000000);
+  var date: LocalDateTime = moment.plusSeconds(1e9.toLong());
 
   constructor(moment:LocalDate): this(moment.atStartOfDay()) {}
 }

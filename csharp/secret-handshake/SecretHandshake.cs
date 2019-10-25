6eda7a3b0aa42364b77bde5dc2207dfeb151d656
Mike S <mstange@dthera.com>
2019-10-25 07:17:27 -0700

    secret-handshake solution

---
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SecretHandshake
+{
+    public static string[] Commands(int commandValue)
+    {
+        var commands = new Dictionary<int, string>(){
+            { 1, "wink" },
+            { 2, "double blink" },
+            { 4, "close your eyes" },
+            { 8, "jump" }
+        };
+        var res = new List<string>();
+        foreach (KeyValuePair<int, string> entry in commands) {
+            if ((entry.Key & commandValue) != 0) {
+                res.Add(entry.Value);
+            }
+        }
+        if ((commandValue & 16) != 0) {
+            res.Reverse();
+        }
+        return res.ToArray();
+    }
+}

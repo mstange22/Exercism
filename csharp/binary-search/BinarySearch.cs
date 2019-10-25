55856c57c5588acac25d13b32bfee14c16faf9c6
Mike S <mstange@dthera.com>
2019-10-25 07:03:36 -0700

    binary-search solution

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,20 @@
+﻿public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        if (input.Length < 1) {
+            return -1;
+        }
+        var min = 0;
+        var max = input.Length - 1;
+        while (min < max) {
+            var mid = (min + max) / 2;
+            if (value > input[mid]) {
+                min = mid + 1;
+            } else {
+                max = mid;
+            }
+        }
+        return input[min] == value ? min : -1;
+    }
+}

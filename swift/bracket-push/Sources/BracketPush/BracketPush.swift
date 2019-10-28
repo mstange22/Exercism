7347b197c8a22918bf43320e7e7b57c07de4b176
Mike S <mstange@dthera.com>
2019-10-28 09:43:06 -0700

    bracket-push solution

---
+++ swift/bracket-push/Sources/BracketPush/BracketPush.swift
@@ -0,0 +1,16 @@
+struct BracketPush {
+    public static func paired(text: String) -> Bool {
+        var brackets = [String]()
+        let bracketMap = ["[": "]", "(": ")", "{": "}"]
+        for char in text {
+            if  bracketMap.keys.contains(String(char)) {
+                brackets.append(bracketMap[String(char)]!)
+            } else if bracketMap.values.contains(String(char)) {
+                if brackets.popLast() != String(char) {
+                    return false
+                }
+            }
+        }
+        return brackets.count == 0
+    }
+}

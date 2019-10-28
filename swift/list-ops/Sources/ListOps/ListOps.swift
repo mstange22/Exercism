65a70c4effee884e6128411727a4523248fccc25
Mike S <mstange@dthera.com>
2019-10-28 13:07:10 -0700

    list-ops solution

--- swift/list-ops/Sources/ListOps/ListOps.swift
+++ swift/list-ops/Sources/ListOps/ListOps.swift
@@ -1,15 +1,71 @@
-class ListOps {
-  public static func append<T>(_ list: [T], _ items: [T]) -> [T] {
-    return list + items
-  }
+import Foundation
 
-  public static func concat<T>(_ list: [T]...) -> [T] {
-    var res = [T]();
-    for l in list {
-      for e in l {
-        res.append(e)
-      }
-    }
-    return res
-  }
+class ListOps {
+
+	public static func append<T>(_ list: [T], _ items: [T]) -> [T] {
+		return list + items
+	}
+
+	public static func concat<T>(_ list: [T]...) -> [T] {
+		var res = [T]();
+
+		for item in list {
+		  for e in item {
+			res.append(e)
+		  }
+		}
+		return res
+	}
+
+	static func map<T>(_ list: [T], _ predicate: (T) -> T) -> [T] {
+		var res = [T]();
+		for item in list {
+			res.append(predicate(item))
+		}
+		return res
+	}
+
+	static func filter<T>(_ list: [T], _ predicate: (T) -> Bool) -> [T] {
+		var res = [T]();
+		for item in list {
+			if predicate(item) {
+				res.append(item)
+			}
+		}
+		return res
+	}
+	
+	static func foldLeft<T>(_ list: [T], accumulated: T, combine: (T, T) -> T) -> T {
+		var res = accumulated
+		for item in list {
+			res = combine(res, item)
+		}
+		return res
+	}
+	
+	static func foldRight<T>(_ list: [T], accumulated: T, combine: (T, T) -> T) -> T {
+		var res = accumulated
+		for item in self.reverse(list) {
+			res = combine(item, res)
+		}
+		return res
+	}
+	
+	static func length<T>(_ list: [T]) -> Int {
+		var len = 0
+		for _ in list {
+			len += 1
+		}
+		return len
+	}
+	
+	static func reverse<T>(_ list: [T]) -> [T] {
+		var res = [T]();
+		var i = list.count - 1
+		while i >= 0 {
+			res.append(list[i])
+			i -= 1
+		}
+		return res
+	}
 }
--- swift/list-ops/Tests/ListOpsTests/ListOpsTests.swift
+++ swift/list-ops/Tests/ListOpsTests/ListOpsTests.swift
@@ -23,65 +23,65 @@ class ListOpsTests: XCTestCase {
         XCTAssertEqual(ListOps.concat([1, 2], [3], [], [4, 5, 6]), [1, 2, 3, 4, 5, 6])
     }
 
-    // func testFilterEmptyList() {
-    //     XCTAssertEqual(ListOps.filter([]) { $0 % 2 == 1 }, [])
-    // }
+     func testFilterEmptyList() {
+         XCTAssertEqual(ListOps.filter([]) { $0 % 2 == 1 }, [])
+     }
 
-    // func testFilterNonemptyList() {
-    //     XCTAssertEqual(ListOps.filter([1, 2, 3, 4, 5]) { $0 % 2 == 1 }, [1, 3, 5])
-    // }
+     func testFilterNonemptyList() {
+         XCTAssertEqual(ListOps.filter([1, 2, 3, 4, 5]) { $0 % 2 == 1 }, [1, 3, 5])
+     }
 
-    // func testLengthEmptyList() {
-    //     XCTAssertEqual(ListOps.length([]), 0)
-    // }
+     func testLengthEmptyList() {
+         XCTAssertEqual(ListOps.length([]), 0)
+     }
 
-    // func testLengthNonemptyList() {
-    //     XCTAssertEqual(ListOps.length([1, 2, 3, 4]), 4)
-    // }
+     func testLengthNonemptyList() {
+         XCTAssertEqual(ListOps.length([1, 2, 3, 4]), 4)
+     }
 
-    // func testMapEmptyList() {
-    //     XCTAssertEqual(ListOps.map([]) { $0 + 1 }, [])
-    // }
+     func testMapEmptyList() {
+         XCTAssertEqual(ListOps.map([]) { $0 + 1 }, [])
+     }
 
-    // func testMapNonemptyList() {
-    //     XCTAssertEqual(ListOps.map([1, 3, 5, 7]) { $0 + 1 }, [2, 4, 6, 8])
-    // }
+     func testMapNonemptyList() {
+         XCTAssertEqual(ListOps.map([1, 3, 5, 7]) { $0 + 1 }, [2, 4, 6, 8])
+     }
 
-    // func testFoldLeftEmptyList() {
-    //     XCTAssertEqual(ListOps.foldLeft([], accumulated: 2, combine: +), 2)
-    // }
+     func testFoldLeftEmptyList() {
+         XCTAssertEqual(ListOps.foldLeft([], accumulated: 2, combine: +), 2)
+     }
 
-    // func testFoldLeftNonemptyListAddition() {
-    //     XCTAssertEqual(ListOps.foldLeft([1, 2, 3, 4], accumulated: 5, combine: +), 15)
-    // }
+     func testFoldLeftNonemptyListAddition() {
+         XCTAssertEqual(ListOps.foldLeft([1, 2, 3, 4], accumulated: 5, combine: +), 15)
+     }
 
-    // func testFoldLeftNonemptyListDivision() {
-    //     XCTAssertEqual(ListOps.foldLeft([2, 5], accumulated: 5, combine: /), 0)
-    // }
+     func testFoldLeftNonemptyListDivision() {
+         XCTAssertEqual(ListOps.foldLeft([2, 5], accumulated: 5, combine: /), 0)
+     }
 
-    // func testFoldRightEmptyList() {
-    //     XCTAssertEqual(ListOps.foldRight([], accumulated: 2, combine: *), 2)
-    // }
+     func testFoldRightEmptyList() {
+         XCTAssertEqual(ListOps.foldRight([], accumulated: 2, combine: *), 2)
+     }
 
-    // func testFoldRightNonemptyListAddition() {
-    //     XCTAssertEqual(ListOps.foldRight([1, 2, 3, 4], accumulated: 5, combine: +), 15)
-    // }
+     func testFoldRightNonemptyListAddition() {
+         XCTAssertEqual(ListOps.foldRight([1, 2, 3, 4], accumulated: 5, combine: +), 15)
+     }
 
-    // func testFoldRightNonemptyListDivision() {
-    //     XCTAssertEqual(ListOps.foldRight([2, 5], accumulated: 5, combine: /), 2)
-    // }
+     func testFoldRightNonemptyListDivision() {
+         XCTAssertEqual(ListOps.foldRight([2, 5], accumulated: 5, combine: /), 2)
+     }
 
-    // func testFoldRightAddString() {
-    //     XCTAssertEqual(ListOps.foldRight(["e", "x", "e", "r", "c", "i", "s", "m"], accumulated: "!", combine: +), "exercism!")
-    // }
+     func testFoldRightAddString() {
+         XCTAssertEqual(ListOps.foldRight(["e", "x", "e", "r", "c", "i", "s", "m"], accumulated: "!", combine: +), "exercism!")
+     }
 
-    // func testReverseEmptyList() {
-    //     XCTAssertEqual(ListOps.reverse([Int]()), [])
-    // }
+     func testReverseEmptyList() {
+         XCTAssertEqual(ListOps.reverse([Int]()), [])
+     }
 
-    // func testReverseNonemptyList() {
-    //     XCTAssertEqual(ListOps.reverse([1, 3, 5, 7]), [7, 5, 3, 1])
-    // }
+     func testReverseNonemptyList() {
+         XCTAssertEqual(ListOps.reverse([1, 3, 5, 7]), [7, 5, 3, 1])
+     }
 
     static var allTests: [(String, (ListOpsTests) -> () throws -> Void)] {
         return [

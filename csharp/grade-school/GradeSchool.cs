83b3e9a0585b955d786b7bfd15658127318aab33
Mike S <mstange@dthera.com>
2019-10-29 08:41:04 -0700

    nested Student class and initializer block

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -1,38 +1,30 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class Student {
-    public int grade;
-    public string name;
-
-    public Student(string name, int grade) {
-        this.name = name;
-        this.grade = grade;
-    }
-}
-
 public class GradeSchool
 {
+    private class Student {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+    
     private List<Student> roster = new List<Student>();
 
     public void Add(string student, int grade)
     {
-        roster.Add(new Student(student, grade));
-        roster.Sort((a, b) => {
-            if (a.grade == b.grade) {
-                return a.name.CompareTo(b.name);
-            }
-            return  a.grade - b.grade;
+        roster.Add(new Student(){
+            Name = student,
+            Grade = grade
         });
     }
 
-    public IEnumerable<string> Roster()
-    {
-        return roster.Select(student => student.name);
-    }
+    public IEnumerable<string> Roster() => roster
+        .OrderBy(student => student.Grade)
+        .ThenBy(student => student.Name)
+        .Select(student => student.Name);
 
-    public IEnumerable<string> Grade(int grade)
-    {
-        return roster.Where(student => student.grade == grade).Select(student => student.name);
-    }
+    public IEnumerable<string> Grade(int grade) => roster
+        .Where(student => student.Grade == grade)
+        .OrderBy(student => student.Name)
+        .Select(student => student.Name);
 }

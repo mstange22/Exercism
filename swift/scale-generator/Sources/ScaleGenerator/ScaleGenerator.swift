a7c0b68da2551d84a7649414cdb36c8d574eadad
Mike S <mstange@dthera.com>
2019-10-29 11:51:58 -0700

    use nested ternary

--- swift/scale-generator/Sources/ScaleGenerator/ScaleGenerator.swift
+++ swift/scale-generator/Sources/ScaleGenerator/ScaleGenerator.swift
@@ -1,7 +1,7 @@
 class ScaleGenerator {
     let cScale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
     let fScale = ["F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"]
-    let useFlats = ["F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"]
+    let useFScale = ["F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"]
     let tonic: String
     let scaleName: String
     let pattern: String
@@ -24,20 +24,13 @@ class ScaleGenerator {
             return tonic == "C" ? cScale : fScale
         }
 
-        let currScale = useFlats.contains(tonic) ? fScale : cScale
+        let currScale = useFScale.contains(tonic) ? fScale : cScale
         var currNoteIndex = currScale.index(of: formattedTonic)!
         var res = [String]()
         
         for step in pattern {
             res.append(currScale[currNoteIndex])
-            var steps: Int
-            if step == "m" {
-                steps = 1
-            } else if step == "M" {
-                steps = 2
-            } else {
-                steps = 3
-            }
+            let steps = step == "m" ? 1 : step == "M" ? 2 : 3
             currNoteIndex = (currNoteIndex + steps) % currScale.count
         }
         return res

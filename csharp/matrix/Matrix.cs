82849df93f5860fb945d42dc5900985e8a1e1d3f
Mike S <mstange@dthera.com>
2019-10-29 08:08:34 -0700

    matrix solution

---
+++ csharp/matrix/Matrix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Matrix
+{
+    private int[][] matrix;
+
+    public Matrix(string input)
+    {
+        this.matrix = input.Split("\n")
+            .Select(s => s.Split(" ")
+                .Select(d => Int32.Parse(d)).ToArray()
+            ).ToArray();
+    }
+
+    public int Rows
+    {
+        get => matrix.Length;
+    }
+
+    public int Cols
+    {
+        get => matrix[0].Length;
+    }
+
+    public int[] Row(int row) => matrix[row - 1];
+
+    public int[] Column(int col) => this.matrix.Select(row => row[col - 1]).ToArray();
+}

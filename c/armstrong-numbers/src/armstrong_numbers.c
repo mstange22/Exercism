aef7f9f570ea351085c5b247538867ef9b51a514
Mike S <mstange@dthera.com>
2019-11-05 07:49:48 -0800

    armstrong_numbers solution

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -5,21 +5,19 @@
 
 bool is_armstrong_number(int n)
 {
-  char digits[MAX_LENGTH];
+  int digits[MAX_LENGTH];
   int original = n;
   int length = 0;
   int sum = 0;
 
   for (int i = 0; i < MAX_LENGTH && n > 0; i++, length++)
   {
-    digits[i] = (n % 10) + '0';
+    digits[i] = (n % 10);
     n /= 10;
   }
 
-  digits[length] = '\0';
-
   for (int i = 0; i < length; i++) {
-    sum += (int)pow(digits[i] - '0', length);
+    sum += (int)pow(digits[i], length);
   }
 
   return original == sum;

cfe660563edf7241b4598ba829abd7f714f06d49
Mike S <mstange@dthera.com>
2019-11-05 11:36:37 -0800

    revised scrabble-score solution

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,17 +1,15 @@
-val scrabbleMap = mapOf(1 to "AEIOULNRST", 2 to "DG", 3 to "BCMP", 4 to "FHVWY", 5 to "K", 8 to "JX", 10 to "QZ")
-
 object ScrabbleScore {
   fun scoreWord(word: String): Int {
-
     return word.toUpperCase().map {
       when (it) {
-        in scrabbleMap[1]!! -> 1
-        in scrabbleMap[2]!! -> 2
-        in scrabbleMap[3]!! -> 3
-        in scrabbleMap[4]!! -> 4
-        in scrabbleMap[5]!! -> 5
-        in scrabbleMap[8]!! -> 8
-        else -> 10
+        'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> 1
+        'D', 'G' -> 2
+        'B', 'C', 'M', 'P' -> 3
+        'F', 'H', 'V', 'W', 'Y' -> 4
+        'K' -> 5
+        'J', 'X' -> 8
+        'Q', 'Z' -> 10
+        else -> 0
       }
     }.sum()
   }

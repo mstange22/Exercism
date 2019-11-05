2d1ef935bd809c37587535168ef9452825abcd9f
Mike S <mstange@dthera.com>
2019-11-05 08:30:10 -0800

    revised scrabble-score solution

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,22 +1,18 @@
-package ScrabbleScore
-
 val scrabbleMap = mapOf(1 to "AEIOULNRST", 2 to "DG", 3 to "BCMP", 4 to "FHVWY", 5 to "K", 8 to "JX", 10 to "QZ")
 
-fun scoreWord(word: String): Int {
-  var score = 0
+object ScrabbleScore {
+  fun scoreWord(word: String): Int {
 
-  // derive one-to-one map for constant time lookup
-  var derivedMap = mutableMapOf<Char, Int>()
-  for ((key, value) in scrabbleMap.entries) {
-    for (letter in value) {
-      derivedMap[letter] = key
-    }
+    return word.toUpperCase().map {
+      when (it) {
+        in scrabbleMap[1]!! -> 1
+        in scrabbleMap[2]!! -> 2
+        in scrabbleMap[3]!! -> 3
+        in scrabbleMap[4]!! -> 4
+        in scrabbleMap[5]!! -> 5
+        in scrabbleMap[8]!! -> 8
+        else -> 10
+      }
+    }.sum()
   }
-
-  for (letter in word.toUpperCase()) {
-    if (derivedMap.containsKey(letter)) {
-      score += derivedMap[letter]!!;
-    }
-  }
-  return score
 }

b7751c8a3316be39d65d5b31d2f0c5e358303adc
Mike S <mstange@dthera.com>
2019-11-18 07:34:54 -0800

    revised difference-of-squares solution

--- kotlin/difference-of-squares/src/main/kotlin/Squares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,8 +1,7 @@
 class Squares(val n: Int) {
-  fun squareOfSum(): Int {
-    var sum = n * (n + 1) / 2
-    return sum * sum
-  }
+  fun squareOfSum(): Int = (n * (n + 1) / 2).square();
   fun sumOfSquares() = n * (n + 1) * (2 * n + 1) / 6
   fun difference() = squareOfSum() - sumOfSquares()
 }
+
+fun Int.square() = this * this

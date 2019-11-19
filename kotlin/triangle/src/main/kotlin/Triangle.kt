02dda31c5d8b7c87bdea5c328a100cb585213b28
Mike S <mstange@dthera.com>
2019-11-19 09:31:42 -0800

    revised triangle solution

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,18 +1,17 @@
 class Triangle<T>(val a: T, val b: T, val c: T) 
     where T : Number, T : Comparable<T> {
+    
+    private val sides: Set<T>
 
     init {
-        if (a == 0 || b == 0 || c == 0) {
-            throw IllegalArgumentException("Not a triangle")
-        }
-
-        val sortedSides = mutableListOf<T>(a, b, c).sorted()
+        val sortedSides = sequenceOf(a, b, c).sorted().toList()
         if (sortedSides[0].toDouble() + sortedSides[1].toDouble() <= sortedSides[2].toDouble()) {
             throw IllegalArgumentException("Not a triangle")
         }
+        sides = sortedSides.toSet()
     }
 
-    val isEquilateral: Boolean = setOf<T>(a, b, c).size == 1
-    val isIsosceles: Boolean = setOf<T>(a, b, c).size <= 2
-    val isScalene: Boolean = setOf<T>(a, b, c).size == 3
+    val isEquilateral: Boolean = sides.size == 1
+    val isIsosceles: Boolean = sides.size <= 2
+    val isScalene: Boolean = sides.size == 3
 }

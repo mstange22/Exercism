18eaa2d9a7b92a222d1a9f3f8474aafb6df32985
Mike S <mstange@dthera.com>
2019-11-20 08:41:53 -0800

    book_store progress

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -10,6 +10,25 @@ group_totals = {
   5: FULL_PRICE * .75
 }
 
+def get_group(book_counts, group_size):
+  # if len(book_counts) < group_size:
+  #   return []
+
+  group = []
+
+  for book in book_counts:
+    # if there are remaining books at this position, add to current group
+    if book[1] > 0:
+      group.append(book[0])
+      book[1] -= 1
+    # stop if reach this current i number of books
+    if len(group) == group_size:
+      break
+  
+  return group
+
+
+
 def calculate_total(books):
   if len(books) == 0:
     return 0
@@ -18,29 +37,37 @@ def calculate_total(books):
   c = Counter(books)
 
   # iterate over all possible series lengths, starting with highest
-  for i in range(max(c.keys()), 0, -1):
+  for series_count in range(len(c.keys()), 0, -1):
     cost = 0
     book_count = 0
     # sorted list of counter key/value pairs
-    temp = [list(x) for x in c.most_common()]
+    working_book_counts = [list(x) for x in c.most_common()]
+
+    # need to test all possible subgroups
+    # curr_test_group_count = series_count
+    # while curr_test_group_count > 1:
 
     # get cost for all books in temp list for this i max grouping value
     while book_count < len(books):
       # get the largest possible group for this i
-      group = []
+      group = get_group(working_book_counts, series_count)
+      book_count += len(group)
       # build the group of up to i books (or however many are left)
-      for book in temp:
-        # if there are remaining books at this position, add to current group
-        if book[1] > 0:
-          group.append(book[0])
-          book[1] -= 1
-          book_count += 1
-        # stop if reach this current i number of books
-        if len(group) == i:
-          break
+      # for book in working_book_counts:
+      #   # if there are remaining books at this position, add to current group
+      #   if book[1] > 0:
+      #     group.append(book[0])
+      #     book[1] -= 1
+      #     book_count += 1
+      #   # stop if reach this current i number of books
+      #   if len(group) == series_count:
+      #     break
       cost += len(group) * group_totals[len(group)]
     # if this cost is lowest, replace current lowest
     if not lowest or cost < lowest:
       lowest = cost
 
   return lowest
+
+# print(calculate_total([2, 2]))
+print(calculate_total([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3]))

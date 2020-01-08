8025434386efb51b075c8bad07c61cdc8dc13782
Mike S <mstange@dthera.com>
2020-01-08 11:05:07 -0800

    phone-number solution

---
+++ typescript/phone-number/phone-number.ts
@@ -0,0 +1,27 @@
+export default class PhoneNumber {
+  private _number: string | undefined;
+
+  constructor(input: string) {
+    this._number = this.validateInput(input);
+  }
+
+  number = (): string | undefined => this._number;
+
+  validateInput = (input: string): string | undefined => {
+    input = input.replace(/[()-. ]/g, '');
+    if (input.length < 10 || input.length > 11) {
+      return undefined;
+    }
+    if (input.length == 11) {
+      if (input.startsWith('1')) {
+        input = input.slice(1);
+      } else {
+        return undefined;
+      }
+    }
+    if (/[^0-9]/.test(input)) {
+      return undefined;
+    }
+    return input;
+  }
+}

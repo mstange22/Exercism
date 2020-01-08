5031839e7b8c55be19d312857f002f24283fa156
Mike S <mstange@dthera.com>
2020-01-08 09:43:53 -0800

    resistor-color-duo solution

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,16 @@
+const COLORS = [
+  'black', 'brown', 'red', 'orange', 'yellow',
+  'green', 'blue', 'violet', 'grey', 'white'
+];
+
+export class ResistorColor {
+  private colors: string[];
+
+  constructor(colors: string[]) {
+    if (colors.length < 2) {
+      throw new Error("At least two colors need to be present");
+    }
+    this.colors = colors;
+  }
+  value = (): number => Number(COLORS.indexOf(this.colors[0]).toString() + COLORS.indexOf(this.colors[1].toString()));
+}

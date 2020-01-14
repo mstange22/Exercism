ecf6d8bad478840d4fd3fb8b423ded8156683886
Mike S <mstange@dthera.com>
2020-01-14 13:03:00 -0800

    use string not array

--- typescript/proverb/proverb.ts
+++ typescript/proverb/proverb.ts
@@ -1,9 +1,9 @@
 const Proverb = (...args: string[]): string => {
-  const res: string[] = []
+  let res = ''
   for (let i = 0; i < args.length - 1; i++) {
-    res.push(`For want of a ${args[i]} the ${args[i + 1]} was lost.`)
+    res += `For want of a ${args[i]} the ${args[i + 1]} was lost.\n`
   }
-  return [...res, `And all for the want of a ${args[0]}.`].join('\n');
+  return res + `And all for the want of a ${args[0]}.`
 }
 
 export default Proverb

7d9c3ddcb4b988ba7f2770c7257a86296544eb7b
Mike S <mstange@dthera.com>
2020-01-19 14:37:05 -0800

    leap solution

---
+++ bash/leap/leap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+main () {
+  if [[ $# != 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]
+  then
+    echo "Usage: leap.sh <year>"
+    exit 1
+  fi
+  if (( $1 % 4 != 0 )) || ((( $1 % 100 == 0)) && (( $1 % 400 != 0)))
+  then echo "false"
+  else echo "true"
+  fi
+}
+
+main "$@"

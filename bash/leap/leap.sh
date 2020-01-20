b0b1d427088dde961e2eb6b45509c9135f2a888b
Mike S <mstange@dthera.com>
2020-01-20 09:58:22 -0800

    indentation

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -2,12 +2,12 @@
 
 main () {
   if [[ $# != 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]
-  then
-    echo "Usage: leap.sh <year>"
-    exit 1
+    then
+      echo "Usage: leap.sh <year>"
+      exit 1
   fi
   if (( $1 % 4 != 0 )) || ((( $1 % 100 == 0)) && (( $1 % 400 != 0)))
-  then echo "false"
+    then echo "false"
   else echo "true"
   fi
 }

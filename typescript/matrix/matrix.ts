1c383a4399434e47bae797343a2cd8252854321f
Mike S <mstange@dthera.com>
2020-02-02 09:31:29 -0800

    revised matrix solution

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -3,19 +3,17 @@ class Matrix {
     columns: number[][]
     constructor(matrix: string) {
         this.columns = []
-        this.rows = matrix.split('\n').map((row, i) => row.split(' ').map((e, j) => {
-            const num = Number(e)
-            if (i === 0) {
-                if (j === 0) {
-                    this.columns = [[num]]
-                } else {
+        this.rows = matrix.split('\n')
+            .map((row, i) => row.split(' ')
+            .map((e, j) => {
+                const num = Number(e)
+                if (i === 0) {
                     this.columns.push([num])
+                } else {
+                    this.columns[j].push(num)
                 }
-            } else {
-                this.columns[j].push(num)
-            }
-            return num
-        }))
+                return num
+            }))
     }
 }
 

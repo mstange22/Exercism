a729fb3cd05a6d39b19c07d843e88e33c44f45c2
Mike S <mstange@dthera.com>
2020-02-03 09:05:43 -0800

    hamming solution

---
+++ bash/hamming/hamming.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+(( $# != 2 )) && { echo "Usage: hamming.sh <string1> <string2>"; exit 1; }
+(( ${#1} != ${#2} )) && { echo "left and right strands must be of equal length"; exit 1; }
+
+d=0
+
+for ((i=0; i<${#1}; i++)); do
+  [[ "${1:i:1}" != "${2:i:1}" ]] && (( d+=1 ))
+done
+
+echo "$d"

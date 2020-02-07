c6d79941f18b80c1ce77c9f0b3071dfac132dc78
Mike S <mstange@dthera.com>
2020-02-06 16:02:18 -0800

    darts solution

--- bash/darts/darts.sh
+++ bash/darts/darts.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 
+throw() { exho "error"; exit 1; }
+
 main () {
-  # echo "params: $1 $2"
+  (( $# == 2 )) || throw
+  ([[ $1 =~ [-.0-9] ]] && [[ $2 =~ [-.0-9] ]]) || throw
 
-  # get distance betwween ($1, $2) and (0, 0)
-  abs_x=0
-  abs_y=0
-  (( $1 < 0 )) && abs_x=$(( $abs_x-$1 )) || abs_x=$(( $1-$abs_x ))
-  (( $2 < 0 )) && abs_y=$(( $abs_y-$2 )) || abs_y=$(( $2-$abs_y ))
-
-  # echo "abs_x: $abs_x"
-  # echo "abs_y: $abs_y"
-  # compare distance to score map 10/5/1/0
-  # d=
-  a=$(bc <<< "scale=0; sqrt($(( $abs_x*$abs_x+$abs_y*$abs_y )))")
-  # echo "$a"
-  if (( a > 10 )); then
+  radius=$(bc <<< "scale=2; sqrt( $1*$1 + $2*$2 )")
+  
+  if [[ $(bc <<< "$radius > 10") -eq 1 ]]; then
     echo "0"
-  elif (( a > 5 )); then
+  elif  [[ $(bc <<< "$radius > 5" ) -eq 1 ]]; then
     echo "1"
-  elif (( a > 1 )); then
+  elif [[ $(bc <<< "$radius > 1" ) -eq 1 ]]; then
     echo "5"
   else
     echo "10"
--- bash/darts/darts_test.sh
+++ bash/darts/darts_test.sh
@@ -38,7 +38,7 @@
 }
 
 @test "Near the centre" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh -0.1 -0.1
     [[ $status -eq 0 ]]
     [[ $output == 10 ]]
@@ -52,42 +52,42 @@
 }
 
 @test "Just outside the inner circle" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh 0.8 -0.8
     [[ $status -eq 0 ]]
     [[ $output == 5 ]]
 }
 
 @test "Just within the middle circle" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh -3.5 3.5
     [[ $status -eq 0 ]]
     [[ $output == 5 ]]
 }   
 
 @test "Just outside the middle circle" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh -3.6 -3.6
     [[ $status -eq 0 ]]
     [[ $output == 1 ]]
 }     
 
 @test "Just within the outer circle" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh -7.0 7.0
     [[ $status -eq 0 ]]
     [[ $output == 1 ]]
 }     
 
 @test "Just outside the outer circle" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh 7.1 -7.1
     [[ $status -eq 0 ]]
     [[ $output == 0 ]]
 }    
 
 @test "Asymmetric position between the inner and middle circles" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh 0.5 -4
     [[ $status -eq 0 ]]
     [[ $output == 5 ]]
@@ -96,28 +96,28 @@
 # bash-specific test: Input validation
 
 @test "invalid args: no args" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh
     [[ $status -eq 1 ]]
     [[  -n $output ]]
 }
 
 @test "invalid args: only 1 arg" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh 10
     [[ $status -eq 1 ]]
     [[  -n $output ]]
 }
 
 @test "invalid args: first arg non-numeric" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh foo 10
     [[ $status -eq 1 ]]
     [[  -n $output ]]
 }
 
 @test "invalid args: second arg non-numeric" {
-    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    # [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash darts.sh 10 bar
     [[ $status -eq 1 ]]
     [[  -n $output ]]

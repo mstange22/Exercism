0534c2b3e3e16dc38aa2a08496304196092d1ead
Mike S <mstange@dthera.com>
2020-04-09 18:09:22 -0700

    robot-name solution

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,12 +1,45 @@
-const names: string[] = [];
-let currentNameIndex = 0;
+const ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+class RobotFactory {
+  names: string[] = [];
+  currentNameIndex = 0;
+
+  constructor() {
+    this.populateNames()
+  }
+
+  shuffleNames = (): void => {
+    for (let i = 0; i < this.names.length - 1; i++) {
+      const randomIndex = Math.floor(Math.random() * (this.names.length - i)) + i
+      let temp = this.names[randomIndex]
+      this.names[randomIndex] = this.names[i]
+      this.names[i] = temp
+    }
+  }
+
+  populateNames = (): void => {
+    for (let i = 0; i < 26; i++) {
+      for (let j = 0; j < 26; j++) {
+        for (let x = 0; x < 10; x++) {
+          for (let y = 0; y < 10; y++) {
+            for (let z = 0; z < 10; z++) {
+              this.names.push(`${ALPHA[i]}${ALPHA[j]}${x}${y}${z}`)
+            }
+          } 
+        }
+      }
+    }
+    this.shuffleNames()
+  }
+
+  getName = (): string => this.names[this.currentNameIndex++]
+}
+
+const FACTORY = new RobotFactory();
 
 class RobotName {
   name: string
   constructor() {
-    if (!names.length) {
-      RobotName.populateNames()
-    }
     this.name = this.getName()
   }
 
@@ -14,31 +47,7 @@ class RobotName {
     this.name = this.getName()
   }
 
-  getName = (): string => {
-    return names[currentNameIndex++]
-  }
-
-  static populateNames = (): void => {
-    const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for (let i = 0; i < 26; i++) {
-      for (let j = 0; j < 26; j++) {
-        for (let x = 0; x < 10; x++) {
-          for (let y = 0; y < 10; y++) {
-            for (let z = 0; z < 10; z++) {
-              names.push(`${alpha[i]}${alpha[j]}${x}${y}${z}`)
-            }
-          } 
-        }
-      }
-    }
-    // shuffle names
-    for (let i = 0; i < names.length - 1; i++) {
-      const randomIndex = Math.floor(Math.random() * (names.length - i)) + i
-      let temp = names[randomIndex]
-      names[randomIndex] = names[i]
-      names[i] = temp
-    }
-  }
+  getName = () => FACTORY.getName()
 }
 
 export default RobotName

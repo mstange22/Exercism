79c3367d26e2f25d0f28ad45c13dc7fe0759486b
Mike S <mstange@dthera.com>
2020-04-15 20:17:13 -0700

    revised secret-handshake solution

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,9 +1,9 @@
-const COMMANDS: [number, string][] = [
-  [0b1, 'wink'],
-  [0b10, 'double blink'],
-  [0b100, 'close your eyes'],
-  [0b1000, 'jump'],
-  [0b10000, 'reverse']
+const COMMANDS: [number, (arr: string[]) => void][] = [
+  [0b1, (arr) => arr.push('wink')],
+  [0b10, (arr) => arr.push('double blink')],
+  [0b100, (arr) => arr.push('close your eyes')],
+  [0b1000, (arr) => arr.push('jump')],
+  [0b10000, (arr) => arr.reverse()]
 ]
 
 class HandShake {
@@ -18,11 +18,7 @@ class HandShake {
     COMMANDS.forEach((commandTuple) => {
       const [bit, command] = commandTuple
       if ((this.n & bit) === bit) {
-        if (bit === 0b10000) {
-          res.reverse();
-        } else {
-          res.push(command)
-        }
+        command(res)
       }
     })
     return res

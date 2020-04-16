7dc1f8b7a6dad949bd657a58786d24c9fe9dd978
Mike S <mstange@dthera.com>
2020-04-16 08:02:20 -0700

    revised binary-search solution

--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -1,37 +1,37 @@
 export default class BinarySearch {
-  arr: number[] = []
+  _array: number[] = []
   isUnordered = false
   
-  constructor(arr: number[]) {
-    for (let i = 0; i < arr.length - 1; i += 1) {
-      if (arr[1] > arr[i + 1]) {
+  constructor(array: number[]) {
+    for (let i = 0; i < array.length - 1; i += 1) {
+      if (array[1] > array[i + 1]) {
         this.isUnordered = true
         return
       }
     }
-    this.arr = arr
+    this._array = array
   }
 
   get array(): number[] | undefined {
-    return this.isUnordered ? undefined : this.arr
+    return this.isUnordered ? undefined : this._array
   }
 
   indexOf(n: number) {
     if (this.isUnordered) {
       return -1
     }
-    let min = 0, max = this.arr.length - 1
+    let min = 0, max = this._array.length - 1
     while (min < max) {
       const mid = Math.floor((min + max) / 2)
-      if (this.arr[mid] === n) {
+      if (this._array[mid] === n) {
         return mid;
       }
-      if (this.arr[mid] > n) {
+      if (this._array[mid] > n) {
         min = mid + 1
       } else {
         max = mid - 1
       }
     } 
-    return this.arr[min] === n ? min : -1
+    return this._array[min] === n ? min : -1
   }
 }

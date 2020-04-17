af5c19b073f45d4f73d5a535df08059d94dc4aa3
Mike S <mstange@dthera.com>
2020-04-16 20:41:07 -0700

    more comments

--- typescript/diamond/diamond.ts
+++ typescript/diamond/diamond.ts
@@ -3,6 +3,8 @@ class Diamond {
     const letterCharCode = letter.charCodeAt(0)
     const diff = letterCharCode - 'A'.charCodeAt(0)
     let top: string[] = []
+
+    // build top half
     for (let i = diff; i >= 0; i--) {
       const currLetter = String.fromCharCode(letterCharCode - i)
       const endSpaces = ' '.repeat(i)
@@ -13,6 +15,8 @@ class Diamond {
       }
       top.push(`${line}${endSpaces}\n`)
     }
+    // take all but last line of top (don't repeat given letter).
+    // Flip it (if anything) and add to bottom.
     return [...top, ...top.slice(0, -1).reverse()].join('')
   }
 }

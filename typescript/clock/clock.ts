050629a705e26699da530359f38dfecd48b3c5b7
Mike S <mstange@dthera.com>
2020-04-18 14:35:01 -0700

    revised clock solution

--- typescript/clock/clock.ts
+++ typescript/clock/clock.ts
@@ -6,13 +6,13 @@ class Clock {
     this.minutes = ((((hours * 60) + minutes) % MINUTES_IN_DAY) + MINUTES_IN_DAY) % MINUTES_IN_DAY
   }
 
-  plus = (minutes: number) => new Clock(0, this.minutes + minutes)
+  plus = (minutes: number): Clock => new Clock(0, this.minutes + minutes)
 
-  minus = (minutes: number) => this.plus(-minutes)
+  minus = (minutes: number): Clock => this.plus(-minutes)
 
-  equals = (c: Clock) => this.minutes === c.minutes
+  equals = (c: Clock): boolean => this.minutes === c.minutes
 
-  toString = () => {
+  toString = (): string => {
     const hours = (Math.floor(this.minutes / 60) % 24).toString()
     const minutes = (this.minutes % 60).toString()
     return `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}`

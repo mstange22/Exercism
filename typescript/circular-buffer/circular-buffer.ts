38aa39c341eec20db3ba409d78142d299cf39886
Mike S <mstange@dthera.com>
2020-04-23 14:17:14 -0700

    circular-buffer solution

---
+++ typescript/circular-buffer/circular-buffer.ts
@@ -0,0 +1,50 @@
+const BufferOverflowError = new Error()
+const BufferEmptyError = new Error()
+
+export default class CircularBuffer<T> {
+  buffer: T[]
+  oldestIndex: number
+  count: number = 0
+
+  constructor(private readonly capacity: number) {
+    this.buffer = new Array(capacity)
+    this.oldestIndex = Math.floor(Math.random() * this.capacity)
+  }
+  
+  read(): T {
+    if (!this.count) {
+      throw BufferEmptyError
+    }
+    const res = this.buffer[this.oldestIndex]
+    this.count--
+    this.oldestIndex = (this.oldestIndex + 1) % this.capacity
+    return res
+  }
+
+  write(element: T): void {
+    if (this.isFull()) {
+      throw BufferOverflowError
+    }
+    this.buffer[(this.oldestIndex + this.count) % this.capacity] = element
+    this.count++
+  }
+
+  forceWrite(element: T): void {
+    if (!this.isFull()) {
+      this.write(element)
+    } else {
+      this.buffer[this.oldestIndex] = element
+      this.oldestIndex = (this.oldestIndex + 1) % this.capacity
+    }
+  }
+
+  clear() {
+    this.count = 0;
+  }
+
+  isFull() {
+    return this.count === this.capacity
+  }
+}
+
+export { BufferOverflowError, BufferEmptyError };

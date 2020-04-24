cea90e655e75b909e2d6d791ca47c77bb8075882
Mike S <mstange@dthera.com>
2020-04-24 13:35:51 -0700

    revised word-search solution

--- typescript/word-search/word-search.ts
+++ typescript/word-search/word-search.ts
@@ -1,21 +1,12 @@
-const nextGridPosition: {[direction: string]: (i: number, j: number) => number[]} = {
-  LTR: (i: number, j: number) => [i, j + 1],
-  RTL: (i: number, j: number) => [i, j - 1],
-  TTB: (i: number, j: number) => [i + 1, j],
-  BTT: (i: number, j: number) => [i - 1, j],
-  TLTBR: (i: number, j: number) => [i + 1, j + 1],
-  BRTTL: (i: number, j: number) => [i - 1, j - 1],
-  BLTTR: (i: number, j: number) => [i - 1, j + 1],
-  TRTBL: (i: number, j: number) => [i + 1, j - 1],
-}
+const DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1], [-1, 1], [1, -1]]
 
 export default class WordSearch {
   constructor(readonly grid: string[]) {}
 
-  searchDirection(word: string, rowNum: number, rowIndex: number, direction: string): number[] | undefined {
+  searchDirection(word: string, rowNum: number, rowIndex: number, direction: number[]): number[] | undefined {
     for (const char of word) {
-      [rowNum, rowIndex] = nextGridPosition[direction](rowNum, rowIndex)
-      if (rowNum < 0 || rowNum === this.grid.length || char !== this.grid[rowNum][rowIndex]) {
+      rowNum += direction[0], rowIndex += direction[1]
+      if (rowNum < 0 || rowNum >= this.grid.length || char !== this.grid[rowNum][rowIndex]) {
         return undefined
       }
     }
@@ -26,7 +17,7 @@ export default class WordSearch {
     for (const [i, row] of this.grid.entries()) {
       for (const [j, char] of [...row].entries()) {
         if (char === word[0]) {
-          for (const direction of Object.keys(nextGridPosition)) {
+          for (const direction of DIRECTIONS) {
             const end = this.searchDirection(word.slice(1), i, j, direction)
             if (end) {
               return { start: [i + 1, j + 1], end }

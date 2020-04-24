8728aa503d8427c9c3d531b120dce70770f19052
Mike S <mstange@dthera.com>
2020-04-24 13:39:01 -0700

    added comments

--- typescript/word-search/word-search.ts
+++ typescript/word-search/word-search.ts
@@ -3,6 +3,7 @@ const DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1], [-1, 1],
 export default class WordSearch {
   constructor(readonly grid: string[]) {}
 
+  // return end coords or undefined
   searchDirection(word: string, rowNum: number, rowIndex: number, direction: number[]): number[] | undefined {
     for (const char of word) {
       rowNum += direction[0], rowIndex += direction[1]
@@ -13,10 +14,12 @@ export default class WordSearch {
     return [rowNum + 1, rowIndex + 1]
   }
 
+  // search every cell in grid, exit early if word is found
   search(word: string): { start: number[], end: number[] } | undefined {
     for (const [i, row] of this.grid.entries()) {
       for (const [j, char] of [...row].entries()) {
         if (char === word[0]) {
+          // potential start.  Search in all directions
           for (const direction of DIRECTIONS) {
             const end = this.searchDirection(word.slice(1), i, j, direction)
             if (end) {
@@ -29,6 +32,7 @@ export default class WordSearch {
     return undefined
   }
 
+  // reduce words down to an object
   public find = (words: string[]) => 
     words.reduce((accum: { [word:string]: { start: number[], end: number[] } | undefined }, word) => {
       accum[word] = this.search(word)

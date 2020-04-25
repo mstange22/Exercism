0ac805a9bafa1773818f81b7045fe18fcc441041
Mike S <mstange22@gmail.com>
2020-04-24 18:26:59 -0700

    added comments

--- typescript/spiral-matrix/spiral-matrix.ts
+++ typescript/spiral-matrix/spiral-matrix.ts
@@ -8,14 +8,21 @@ export default class SpiralMatrix {
       res.push(new Array(n))
     }
     
+    // wrap around matrix, filling in cells in order
     for (let i = 0; i < n / 2; i++) {
-      for (let j = i; j < n - i; j++) res[i][j] = value++
+      // left-to-right
+      for (let j = i; j < n - i; j++) {
+        res[i][j] = value++
+      }
+      // down
       for (let j = i + 1; j < n - i; j++) {
         res[j][n - i - 1] = value++
       }
+      // right-to-left
       for (let j = n - i - 2; j >= i; j--) {
         res[n - i - 1][j] = value++
       }
+      // up
       for (let j = n - i - 2; j > i; j--) {
         res[j][i] = value++
       }

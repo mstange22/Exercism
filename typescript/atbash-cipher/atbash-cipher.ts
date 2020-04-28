8d5d82217042968f30320ac98f5a26011491c0c5
Mike S <mstange22@gmail.com>
2020-04-27 17:21:41 -0700

    revised atbash cipher solution

--- typescript/atbash-cipher/atbash-cipher.ts
+++ typescript/atbash-cipher/atbash-cipher.ts
@@ -1,40 +1,11 @@
-const isAlphaNum = (c: string) => {
-  return /[A-Za-z0-9]/.test(c)
-}
-
-const isNumber = (c: string) => {
-  return /[0-9]/.test(c)
-}
-
-const letterPosition = (c: string) => {
-  return c.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0)
-}
-
-const translateLetter = (c: string) => {
-  return String.fromCharCode('z'.charCodeAt(0) - letterPosition(c))
-}
-
-const addSpaces = (s: string): string => s.replace(/(.{5})(?=\S)/g, '$1 ')
+const isAlphaNum = (c: string) => /[A-Za-z0-9]/.test(c)
+const isNumber = (c: string) => /[0-9]/.test(c)
+const letterPosition = (c: string) => c.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0)
+const translateLetter = (c: string) => String.fromCharCode('z'.charCodeAt(0) - letterPosition(c))
+const addSpaces = (s: string, chunkSize: number = 5) => [...s].reduce((accum: string[], _, i) => i % chunkSize === 0 ? [...accum, s.substring(i, i + chunkSize)] : accum, []).join(' ')
 
 export default class AtbashCipher {
-  translateString = (s: string) => {
-    return [...s].reduce((accum, c) => {
-      if (isAlphaNum(c)) {
-        if (isNumber(c)) {
-          accum += c
-        } else {
-          accum += translateLetter(c)
-        }
-      }
-      return accum
-    }, '')
-  }
-
-  encode = (s: string) => {
-    return addSpaces(this.translateString(s))
-  }
-
-  decode = (s: string) => {
-    return this.translateString(s)
-  }
+  translateString = (s: string) => [...s].reduce((accum, c) => isAlphaNum(c) ? isNumber(c) ? accum + c : accum + translateLetter(c) : accum, '')
+  encode = (s: string) => addSpaces(this.translateString(s))
+  decode = (s: string) => this.translateString(s)
 }

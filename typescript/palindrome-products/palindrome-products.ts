297a5ed23019614f9f1952572fdf78aefd2afdc4
Mike S <mstange22@gmail.com>
2020-04-28 15:48:43 -0700

    initial palindrome-products solution

---
+++ typescript/palindrome-products/palindrome-products.ts
@@ -0,0 +1,38 @@
+const isPalindrome = (n: number): boolean => {
+  const s = n.toString()
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    if (s[i] !== s[s.length - 1 - i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const generate = (args: { maxFactor: number, minFactor?: number }): { [key: string]: { value: number, factors: number[][] } } => {
+  const { maxFactor, minFactor = 1} = args
+  let largest: { value: number, factors: number[][] } = { value: 0, factors: [] }
+  let smallest: { value: number, factors: number[][] } = { value: Infinity, factors: [] }
+
+  for (let i = minFactor; i <= maxFactor; i++) {
+    for (let j = minFactor; j <= maxFactor; j++) {
+      const product = i * j
+      if (isPalindrome(product)) {
+        if (product > largest.value) {
+          largest.value = product
+          largest.factors = [[i, j]]
+        } else if (product === largest.value && largest.factors.every(f => f[0] != j)) {
+          largest.factors.push([i, j])
+        }
+        if (product < smallest.value) {
+          smallest.value = product
+          smallest.factors.push([i, j])
+        } else if (product === smallest.value && smallest.factors.every(f => f[0] !== j)) {
+          smallest.factors.push([i, j])
+        }
+      }
+    }
+  }
+  return { largest, smallest }
+}
+
+export default generate

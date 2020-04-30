017213394b422791874f9ce61d9eecbf2f8f4a6a
Mike S <mstange22@gmail.com>
2020-04-30 11:59:40 -0700

    refactored simple-cipher soluiton

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -1,3 +1,6 @@
+const A_CODE = 'a'.charCodeAt(0)
+const NUM_LETTERS = 26
+
 enum Coding {
     Encoding,
     Decoding
@@ -6,10 +9,12 @@ enum Coding {
 const generateKey = () => {
     let res = ''
     for (let i = 0; i < 100; i++) {
-        res += String.fromCharCode(Math.floor(Math.random() * 26) + 'a'.charCodeAt(0))
+        res += String.fromCharCode(Math.floor(Math.random() * NUM_LETTERS) + A_CODE)
     }
     return res
 }
+
+const normalizeShift = (shift: number) => ((((shift) - A_CODE) % NUM_LETTERS) + NUM_LETTERS) % NUM_LETTERS
 
 class SimpleCipher {
     key: string
@@ -18,19 +23,20 @@ class SimpleCipher {
         this.key = key || generateKey()
     }
 
+    getKeyShift = (i: number, coding: Coding): number => {
+        const keyCode = this.key[i % this.key.length].charCodeAt(0)
+        return coding === Coding.Decoding ? A_CODE - keyCode : keyCode - A_CODE
+    }
+
+    getNetShift = (c: string, i: number, coding: Coding) => normalizeShift(c.charCodeAt(0) + this.getKeyShift(i, coding))
+
+    translateChar = (c: string, i: number, coding: Coding) => String.fromCharCode(A_CODE + this.getNetShift(c, i, coding))
+
     translate = (text: string, coding: Coding) =>
-        [...text.toLowerCase()].reduce((accum, c, i) => {
-            const aCode = 'a'.charCodeAt(0)
-            const keyCode = this.key[i % this.key.length].charCodeAt(0)
-            const charCode = c.charCodeAt(0)
-            let diff = keyCode - aCode
-            if (coding === Coding.Decoding) {
-                diff = -diff
-            }
-            return accum + String.fromCharCode((((((charCode + diff) - aCode) % 26) + 26) % 26) + aCode)
-        }, '')
+        [...text.toLowerCase()].reduce((accum, c, i) => accum + this.translateChar(c, i, coding), '')
 
     encode = (plaintext: string) => this.translate(plaintext, Coding.Encoding)
+
     decode = (ciphertext: string) => this.translate(ciphertext, Coding.Decoding)
 }
 

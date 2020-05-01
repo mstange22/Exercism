dd31562968cea1997cea2a865ffb5ce7ff8ccaba
Mike S <mstange22@gmail.com>
2020-05-01 14:54:25 -0700

    revised kindergarten-garten solution

--- kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -5,13 +5,11 @@ val nucleotideCounts = mutableMapOf('A' to 0, 'C' to 0, 'G' to 0, 'T' to 0)
 class KindergartenGarden(private val diagram: String) {
 
     fun getPlantsOfStudent(student: String): List<String> {
+
         var index = (student.get(0).toUpperCase().toInt() - 'A'.toInt()) * 2
-        var garden = diagram.split('\n')
-        return listOf(
-            GardenMap.getOrDefault(garden[0][index], ""),
-            GardenMap.getOrDefault(garden[0][index + 1], ""),
-            GardenMap.getOrDefault(garden[1][index], ""),
-            GardenMap.getOrDefault(garden[1][index + 1], "")
-        )
+
+        return diagram.split('\n').fold(listOf<String>()) { accum, row -> accum + 
+            GardenMap.getOrDefault(row[index], "") + GardenMap.getOrDefault(row[index + 1], "")
+        }
     }
 }

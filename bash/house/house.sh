2f40257a6690f05d0cee9b27395415f33b11a386
Mike S <mstange22@gmail.com>
2020-05-03 15:29:46 -0700

    initial house solution

--- bash/house/house.sh
+++ bash/house/house.sh
@@ -1,24 +1,41 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+declare -A parts=(
+  ["malt"]="lay in"
+  ["rat"]="ate"
+  ["cat"]="killed"
+  ["dog"]="worried"
+  ["cow with the crumpled horn"]="tossed"
+  ["maiden all forlorn"]="milked"
+  ["man all tattered and torn"]="kissed"
+  ["priest all shaven and shorn"]="married"
+  ["rooster that crowed in the morn"]="woke"
+  ["farmer sowing his corn"]="kept"
+  ["horse and the hound and the horn"]="belonged to"
+)
+
+map=(
+  [0]="house that Jack built."
+  [1]="malt"
+  [2]="rat"
+  [3]="cat"
+  [4]="dog"
+  [5]="cow with the crumpled horn"
+  [6]="maiden all forlorn"
+  [7]="man all tattered and torn"
+  [8]="priest all shaven and shorn"
+  [9]="rooster that crowed in the morn"
+  [10]="farmer sowing his corn"
+  [11]="horse and the hound and the horn"
+)
+
+# validate verses
+(( ${@:1:1} < 1 || ${@:1:1} > 12 || ${@:2:1} < 1 || ${@:2:1} > 12  )) && { echo *invalid*; exit 1; }
+
+for (( i=${@:1:1}; i <= ${@:2:1}; i++ )); do
+  echo "This is the ${map[i-1]}"
+  for (( j=i-1; j > 0; j--)); do
+    echo "that ${parts[${map[j]}]} the ${map[j - 1]}"
+  done
+  echo ""
+done

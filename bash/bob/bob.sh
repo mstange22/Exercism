22ba326e3180bd46228260e2ca78a383c58607a8
Mike S <mstange22@gmail.com>
2020-05-05 14:21:44 -0700

    bob solution

---
+++ bash/bob/bob.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+s=${1//[[:space:]]/}
+
+[[ $s == "" ]] && { echo "Fine. Be that way!"; exit 0; }
+
+shouting='^.*!$'
+question='^.*\?$'
+letters='[a-zA-Z]+'
+
+if [[ $s =~ $question ]]; then
+  [[ $s =~ ${s^^} && $s =~ $letters ]] && { echo "Calm down, I know what I'm doing!"; exit 0; }
+  echo "Sure."
+  exit 0
+fi
+
+[[ $s == ${s^^} && $s =~ $letters ]] && { echo "Whoa, chill out!"; exit 0; }
+
+echo "Whatever."

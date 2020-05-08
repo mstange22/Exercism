1e8202a1716f8d6dfb942071d903191e0cce6078
Mike S <mstange22@gmail.com>
2020-05-08 11:52:45 -0700

    revised square_root solution

--- c/square-root/src/square_root.c
+++ c/square-root/src/square_root.c
@@ -1,10 +1,9 @@
 #include "square_root.h"
 
 int squareRoot(int num) {
-  for (int i = 1; i * i <= num; i++) {
-    if (i * i == num) {
-      return i;
-    }
+  float res = 100;
+  while (res * res != num) {
+    res = (res + ((float)num / res)) / 2;
   }
-  return 1;
+  return (int)res;
 }

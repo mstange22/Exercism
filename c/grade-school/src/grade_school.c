991b4ff659649f6a7ba7d62841bec3b81b5ea857
Mike S <mstange22@gmail.com>
2020-05-15 10:36:00 -0700

    revised grade-school solution

--- c/grade-school/src/grade_school.c
+++ c/grade-school/src/grade_school.c
@@ -2,13 +2,12 @@
 #include <string.h>
 
 static roster_t roster = { 0 };
-static roster_t grade_roster = { 0 };
 
 roster_t get_roster() {
   return roster;
 }
 
-int compare_student(student_t student_1, student_t student_2) {
+static int compare_student(student_t student_1, student_t student_2) {
   if (student_1.grade < student_2.grade) {
     return -1;
   }
@@ -18,7 +17,7 @@ int compare_student(student_t student_1, student_t student_2) {
   return strcmp(student_1.name, student_2.name);
 }
  
-void insert_student(int index, student_t student) {
+static void insert_student(int index, student_t student) {
   student_t temp_student;
 
   for (size_t i = index; i < roster.count; i++) {
@@ -42,6 +41,7 @@ int add_student(char *name, uint8_t grade) {
 }
 
 roster_t get_grade(uint8_t grade) {
+  roster_t grade_roster = { 0 };
   for (size_t i = 0; i < roster.count; i++) {
     if (roster.students[i].grade == grade) {
       grade_roster.students[grade_roster.count++] = roster.students[i];
--- c/grade-school/src/grade_school.h
+++ c/grade-school/src/grade_school.h
@@ -18,8 +18,6 @@ typedef struct {
 } roster_t;
 
 roster_t get_roster();
-int compare_student(student_t student_1, student_t student_2);
-void insert_student(int index, student_t student);
 int add_student(char *name, uint8_t grade);
 roster_t get_grade(uint8_t grade);
 void clear_roster();

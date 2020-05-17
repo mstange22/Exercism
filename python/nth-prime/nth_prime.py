cffcccba75c0c65bd397a50c87e08fc7e7aed8ba
Mike S <mstange22@gmail.com>
2020-05-17 12:46:15 -0700

    nth_prime solution

---
+++ python/nth-prime/nth_prime.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+def is_prime(n: int) -> bool:
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def prime(number: int) -> int:
+    if number < 1:
+        raise ValueError("Error: Number must be positive")
+    primes = [2]
+    test_val = 3
+
+    while len(primes) <= number:
+        if is_prime(test_val):
+            primes.append(test_val)
+        test_val += 1
+
+    return primes[number - 1]

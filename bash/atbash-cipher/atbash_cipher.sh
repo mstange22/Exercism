233065443c69ccb1720b47d8c33fcfd3026d82a0
Mike S <mstange22@gmail.com>
2020-06-03 14:54:20 -0700

    revised atbash-cipher solution

--- bash/atbash-cipher/atbash_cipher.sh
+++ bash/atbash-cipher/atbash_cipher.sh
@@ -5,7 +5,7 @@ declare -A alpha_map=()
 
 populate_map() {
   for (( i=0; i<26; i++ )); do
-    alpha_map[${alpha:i:1}]=$i
+    alpha_map[${alpha:i:1}]=${alpha:25-$i:1}
   done
 }
 
@@ -16,17 +16,15 @@ main () {
   cleaned=${cleaned//[^[:alnum:]]/}
 
   res=""
-  count=0
 
   for (( i=0; i<${#cleaned}; i++)); do
     char=${cleaned:i:1}
     if [[ $char == [a-z] ]]; then
       # add space if encoding and have 5 char in current chunk
-      [[ $1 == "encode" ]] && (( count%6 == 5 )) && { res+=" "; (( count++ )); }
-      char=${alpha:25-${alpha_map[$char]}:1}
+      [[ $1 == "encode" ]] && (( i%5 == 0 && i != 0 )) && res+=" "
+      char=${alpha_map[$char]}
     fi
     res+=$char
-    (( count++ ))
   done
 
   echo "$res"

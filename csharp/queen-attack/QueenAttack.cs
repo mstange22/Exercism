4ad785f6e31baf4f63d39141e04d6bc72b41b23b
Mike S <mstange22@gmail.com>
2020-06-03 19:05:07 -0700

    queen-attack solution

---
+++ csharp/queen-attack/QueenAttack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        return white.Row == black.Row || white.Column == black.Column ||
+            Math.Abs(white.Row - black.Row) == Math.Abs(white.Column - black.Column);
+    }
+
+    public static Queen Create(int row, int column)
+    {
+        if (row < 0 || row > 7 || column < 0 || column > 7) {
+            throw new ArgumentOutOfRangeException("bad stuff");
+        }
+        return new Queen(row, column);
+    }
+}

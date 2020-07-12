c0c9bce7f68e7619e97f8bce4351af55c4e3cd9f
Mike S <mstange22@gmail.com>
2020-07-12 13:00:20 -0700

    revised clock solution

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -1,35 +1,20 @@
-using System;
-
 public class Clock
 {
     int minutes;
+    private const int MinutesInDay = 60 * 24;
 
     public Clock(int hours, int minutes)
     {
-        this.minutes = ((((hours % 24) * 60 + (minutes % (60 * 24))) % (24 * 60)) + (60 * 24)) % (60 * 24);
+        this.minutes = ((((hours * 60) + minutes) % MinutesInDay) + MinutesInDay) % MinutesInDay;
     }
 
-    public Clock Add(int minutesToAdd)
-    {
-        return new Clock(0, minutes + minutesToAdd);
-    }
+    public Clock Add(int minutesToAdd) => new Clock(0, minutes + minutesToAdd);
 
-    public Clock Subtract(int minutesToSubtract)
-    {
-        return Add(-minutesToSubtract);
-    }
+    public Clock Subtract(int minutesToSubtract) => Add(-minutesToSubtract);
     
-    public override string ToString()
-    {
-        return $"{(minutes / 60).ToString("D2")}:{(minutes % 60).ToString("D2")}";
-    }
+    public override string ToString() => $"{(minutes / 60):D2}:{(minutes % 60):D2}";
 
-    public override bool Equals(object obj)
-    {
-        return this.ToString() == obj.ToString();
-    }
+    public override bool Equals(object obj) => this.ToString() == obj.ToString();
 
-    public override int GetHashCode() {
-        return this.ToString().GetHashCode();
-    }
+    public override int GetHashCode() => this.ToString().GetHashCode();
 }

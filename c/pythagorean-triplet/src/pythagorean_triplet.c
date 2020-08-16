f3dcb953fb0253954cef037ea89bf1a950dad132
Mike S <mstange22@gmail.com>
2020-08-16 09:40:55 -0700

    revised pythagorean_triplet solution

--- c/pythagorean-triplet/src/pythagorean_triplet.c
+++ c/pythagorean-triplet/src/pythagorean_triplet.c
@@ -3,23 +3,25 @@
 #include <string.h>
 
 triplets_t *triplets_with_sum(uint16_t sum) {
-  triplets_t *res = (triplets_t*)malloc(sizeof(triplets_t));
-  if (!res) return res;
-
   size_t count = 0;
+  triplet_t *triplets = malloc(sizeof(triplet_t*));
+  if (!triplets) return NULL;
 
   for (size_t a = 1; a < sum / 3; a++) {
     for (size_t b = a + 1; b < sum - a - b; b++) {
       size_t c = sum - a - b;
       if (a * a + b * b == c * c) {
         count++;
-        res = (triplets_t*)realloc(res, sizeof(triplets_t) + count * sizeof(triplet_t));
-        if (!res) return res;
-        memcpy(&(res->triplets[count - 1]), &(triplet_t){a, b, c}, sizeof(triplet_t));
+        triplets = realloc(triplets, count * sizeof(triplet_t));
+        triplets[count - 1] = (triplet_t){a, b, c};
       }
     }
   }
+  triplets_t *res = (triplets_t*)malloc(sizeof(triplets_t) + count * sizeof(triplet_t));
+  if (!res) return NULL;
   res->count = count;
+  memcpy(res->triplets, triplets, count * sizeof(triplet_t));
+  free(triplets);
   return res;
 }
 

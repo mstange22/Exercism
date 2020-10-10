2f7cbe54d2b92cdd45a14c9e12d0fb7c7c619540
Mike S <mstange22@gmail.com>
2020-10-10 15:55:55 -0700

    revised word-count solution using fold

--- kotlin/word-count/src/main/kotlin/WordCount.kt
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -1,22 +1,20 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        var map =  mutableMapOf<String, Int>()
-        phrase.split("(,|\\s)+".toRegex()).forEach { word ->
-            var token = word.replace("[\\s,.:!&@$%^&=\"]".toRegex(), "").toLowerCase()
-            if (token.length > 0) {
-                if (token[0] == '\'' && token[token.length - 1] == '\'') {
-                    token = token.substring(1, token.length - 1)
+        return phrase.split("(,|\\s)+".toRegex())
+            .fold(mutableMapOf<String, Int>()) { map, word ->
+                var token = word
+                    .replace("[\\s,.:!&@$%^&=\"]".toRegex(), "")
+                    .toLowerCase()
+                if (token != "") {
+                    // check for single-quoted word (keep apostrophes)
+                    if (token.startsWith('\'') && token.endsWith('\'')) {
+                        token = token.substring(1, token.length - 1)
+                    }
+                    var count = map[token]
+                    map.put(token, if (count != null) count + 1 else 1)
                 }
-                var count = map[token]
-                if (count !== null) {
-                    count += 1
-                } else {
-                    count = 1
-                }
-                map.put(token, count)
+                map
             }
-        }
-        return map
     }
 }

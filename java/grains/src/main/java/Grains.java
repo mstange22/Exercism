5dd00ad8fa83a3db6398c5d5e994873ce7550713
Mike S <mstange22@gmail.com>
2020-10-10 14:38:37 -0700

    construct one for repeated use

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -2,15 +2,17 @@ import java.math.BigInteger;
 
 class Grains {
 
+    BigInteger one = new BigInteger("1");
+
     BigInteger grainsOnSquare(final int square) {
         if (square < 1 || square > 64) {
             throw new IllegalArgumentException("square must be between 1 and 64");
         }
-        return new BigInteger("1").shiftLeft(square - 1);
+        return one.shiftLeft(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-        return new BigInteger("1").shiftLeft(64).subtract(BigInteger.valueOf(1));
+        return one.shiftLeft(64).subtract(one);
     }
 
 }

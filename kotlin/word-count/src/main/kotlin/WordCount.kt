08fe39099a4a942fe5e7eed3ad3bf44cc99f15a6
Mike S <mstange22@gmail.com>
2020-10-11 08:27:35 -0700

    simple regex solution

--- kotlin/word-count/src/main/kotlin/WordCount.kt
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -1,19 +1,11 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        return phrase.split("(,|\\s)+".toRegex())
-            .fold(mutableMapOf<String, Int>()) { map, word ->
-                var token = word
-                    .replace("[\\s,.:!&@$%^&=\"]".toRegex(), "")
-                    .toLowerCase()
-                if (token != "") {
-                    // check for single-quoted word (keep apostrophes)
-                    if (token.startsWith('\'') && token.endsWith('\'')) {
-                        token = token.substring(1, token.length - 1)
-                    }
-                    var count = map[token]
-                    map.put(token, if (count != null) count + 1 else 1)
-                }
+        var wordRegex = "[a-z0-9]+('*[a-z0-9]+)*".toRegex()
+        return wordRegex.findAll(phrase.toLowerCase())
+            .fold(mutableMapOf<String, Int>()) { map, match ->
+                var count = map[match.value]
+                map.put(match.value, if (count != null) count + 1 else 1)
                 map
             }
     }

78c21637515cffb1a69204a88ce187a580ec4d1e
Mike S <mstange22@gmail.com>
2020-10-13 11:01:54 -0700

    revised rotational-cipher solution

--- kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -3,12 +3,12 @@ class RotationalCipher(private val rotation: Int) {
     private fun translate(c: Char): Char {
         if (!c.isLetter()) return c
         val letterValue = c.toLowerCase().toInt() - 'a'.toInt()
-        val newLetterValue = ((letterValue + rotation) % 26)
+        val newLetterValue = (letterValue + rotation + 26) % 26
         val netOffset = newLetterValue - letterValue
         return c + netOffset
     }
 
     fun encode(text: String) = text
-        .map { it -> translate(it) }
+        .map(::translate)
         .joinToString("")
 }
